8801f2d6c02cb2e7e80a3ce4bc6914f685a7af96
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-02-07 21:33:52 +0900

    [level 2] Title: 롤케이크 자르기, Time: 165.16 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,19 +1,16 @@
 function solution(topping) {
     let result=0
-    const left = new Set()
     const right = new Map()
     for(const t of topping){
-        right.set(t,(right.get(t) || 0)+1)
+        right.set(t,(right.get(t)||0)+1)
     }
-    
+    const left = new Set()
     for(const t of topping){
         left.add(t)
         right.set(t,right.get(t)-1)
-        
         if(right.get(t)===0){
             right.delete(t)
         }
-        
         if(left.size===right.size){
             result+=1
         }
