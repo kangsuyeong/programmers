68429e5b1b43600d386731ba45c2808509840c32
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-02-07 21:14:49 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,43 +1,19 @@
-// function solution(keyinput, board) {
-//     let result=[0,0]
-//     for(const f of keyinput){
-//         switch(f){
-//             case "left": if(result[0]>-(board[0]-1)/2) result[0]--; break;
-//             case "right":if(result[0]<(board[0]-1)/2) result[0]++; break;
-//             case "up":if(result[1]<(board[1]-1)/2) result[1]++; break;
-//             case "down":if(result[1]>-(board[1]-1)/2) result[1]--; break;
-//         }
-//     }
-//     return result
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-function solution(keyinput,board){
-    let x=0,y=0
+function solution(keyinput, board) {
+    let x=0
+    let y=0
     const n = Math.floor(board[0]/2)
     const m = Math.floor(board[1]/2)
-    const direction ={
-        left : [-1,0],
-        right : [1,0],
-        up : [0,1],
-        down : [0,-1]
+    const direction={
+        left:[-1,0],
+        right:[1,0],
+        up:[0,1],
+        down:[0,-1]
     }
     function isRange(x,y){
-        return -n<=x && n>=x && -m<=y && m>=y
+        return -n<=x && x<=n && -m<=y && m>=y
     }
     for(const key of keyinput){
-        const [dx,dy] = direction[key]
+        [dx,dy] = direction[key]
         const nx = x + dx
         const ny = y + dy
         if(isRange(nx,ny)){
