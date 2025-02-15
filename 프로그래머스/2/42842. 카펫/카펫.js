763614520141d33655f2682fe73c8ef0a816e72d
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-02-15 16:34:36 +0900

    [level 2] Title: 카펫, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,17 @@
 function solution(brown, yellow) {
-    const block = brown + yellow // 전체 블록 갯수
-    let yellow_h = 1 // 노란 블럭의 높이
+    const block = yellow + brown // 전체 블록갯수
+    let yellow_h = 1 // 노란색의 높이
+    
     while(true){
+        // 너비가 정수 일때만
         if(yellow%yellow_h===0){
-            const yellow_w = yellow/yellow_h
-            if(block === (yellow_w+2)*(yellow_h+2)){
+            const yellow_w = yellow/yellow_h // 노란색의 너비
+            // 노란색의 너비+2 X 노란색의 높이+2가 전체 블록의 갯수일때
+            if(block===(yellow_h+2)*(yellow_w+2)){
                 return [yellow_w+2,yellow_h+2]
             }
         }
-        yellow_h++
+        yellow_h++ // 높이 업데이트하기
     }
     
 }
