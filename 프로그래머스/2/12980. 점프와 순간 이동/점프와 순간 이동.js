3f7bead44f771ae4006fe0939f7d6275ff57091a
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-02-15 16:59:50 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,15 +1,13 @@
 function solution(n){
-    let result=0
+    let battery=0
     while(n>0){
-        // n이 짝수일때
         if(n%2===0){
             n/=2
         }
-        // n이 홀수일때
         else{
             n-=1
-            result+=1
+            battery+=1
         }
     }
-    return result
+    return battery
 }
