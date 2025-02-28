c36202795169bddb7bbb9bda0cb70485e5b367db
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-02-28 21:49:26 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 55.93 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -1,29 +1,29 @@
 function solution(board){
-    const n= board.length
+    const n = board.length
     const m = board[0].length
-    let maxlen = 0
     let dp = Array.from({length:n},()=>Array(m).fill(0))
+    let maxLen = 0
     
     for(let i=0;i<n;i++){
         if(board[i][0]===1){
             dp[i][0]=1
-            maxlen=1
+            maxLen=1
         }
     }
     
     for(let i=0;i<m;i++){
         if(board[0][i]===1){
             dp[0][i]=1
-            maxlen=1
+            maxLen=1
         }
     }
     for(let i=1;i<n;i++){
-        for(let j=1;j<n;j++){
+        for(let j=1;j<m;j++){
             if(board[i][j]===1){
-                dp[i][j] = Math.min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])+1
-                maxlen = Math.max(maxlen,dp[i][j])
+                dp[i][j] = Math.min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j]) +1
+                maxLen = Math.max(maxLen,dp[i][j])
             }
         }
     }
-    return maxlen * maxlen
+    return maxLen * maxLen
 }
