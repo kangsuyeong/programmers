9c6b1baee0336da43c190bc42c9f987ce60f37cc
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-03-08 15:45:32 +0900

    [level 2] Title: 땅따먹기, Time: 63.48 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -2,23 +2,22 @@ function solution(land) {
     const n = land.length
     const dp = Array.from({length:n},()=>Array(4).fill(0))
     
-    // dp 첫번째열 셋팅
     for(let i=0;i<4;i++){
         dp[0][i] = land[0][i]
     }
     for(let i=1;i<n;i++){
         for(let j=0;j<4;j++){
             if(j===0){
-                dp[i][j] = Math.max(dp[i-1][1],dp[i-1][2],dp[i-1][3]) + land[i][j]
+                dp[i][j]=Math.max(dp[i-1][1],dp[i-1][2],dp[i-1][3]) + land[i][j]
             }
             else if(j===1){
-                dp[i][j] = Math.max(dp[i-1][0],dp[i-1][2],dp[i-1][3]) + land[i][j]
+                dp[i][j]=Math.max(dp[i-1][0],dp[i-1][2],dp[i-1][3]) + land[i][j]
             }
             else if(j===2){
-                dp[i][j] = Math.max(dp[i-1][0],dp[i-1][1],dp[i-1][3]) + land[i][j]
+                dp[i][j]=Math.max(dp[i-1][0],dp[i-1][1],dp[i-1][3]) + land[i][j]
             }
             else{
-                dp[i][j] = Math.max(dp[i-1][0],dp[i-1][1],dp[i-1][2]) + land[i][j]
+                dp[i][j]=Math.max(dp[i-1][0],dp[i-1][1],dp[i-1][2]) + land[i][j]
             }
         }
     }
