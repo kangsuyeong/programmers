daf80a63e9aa596a67cfc1ae9218aaa0df44221a
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-03-08 16:06:25 +0900

    [level 4] Title: 단어 퍼즐, Time: 51.35 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.js
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.js
@@ -1,15 +1,16 @@
 function solution(strs, t) {
     const n = t.length
     const dp = Array(n+1).fill(Infinity)
+    dp[0]=0
     
-    dp[0]=0 // 초기값 초기화
-    for(let end=1;end<=n;end++){
-        for(let start=Math.max(1,end-5);start<=end;start++){
-            const currentString = t.slice(start-1,end)
-            if(strs.includes(currentString)){
-                dp[end] = Math.min(dp[end],dp[start-1]+1)
+    for(let i=1;i<=n;i++){
+        for(let j=Math.max(1,i-5);j<=i;j++){
+            const cur_str = t.slice(j-1,i)
+            if(strs.includes(cur_str)){
+                dp[i] = Math.min(dp[i],dp[j-1]+1)
             }
         }
     }
-    return dp[n]===Infinity ? -1 :dp[n]
+    
+    return dp[n]===Infinity ? -1 : dp[n]
 }
