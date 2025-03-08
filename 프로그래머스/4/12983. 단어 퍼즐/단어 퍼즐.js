e10584f947dd56fb51433ef4176e3c0e6cc152af
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-03-08 14:42:01 +0900

    [level 4] Title: 단어 퍼즐, Time: 51.40 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.js
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.js
@@ -1,19 +1,15 @@
 function solution(strs, t) {
-    const n = t.length;
-    let dp = Array(n + 1).fill(Infinity);
-    const set = new Set(strs);
-    const maxLen = Math.max(...strs.map(word => word.length)); // 🔥 가장 긴 단어 길이 찾기
-
-    dp[0] = 0;
-
-    for (let i = 1; i <= n; i++) {
-        for (let j = Math.max(0, i - maxLen); j < i; j++) { // 🔥 불필요한 j 탐색 줄이기
-            const currentString = t.substring(j, i);
-            if (set.has(currentString)) {
-                dp[i] = Math.min(dp[j] + 1, dp[i]);
+    const n = t.length
+    const dp = Array(n+1).fill(Infinity)
+    
+    dp[0]=0 // 초기값 초기화
+    for(let i=1;i<=n;i++){
+        for(let j=Math.max(0,i-5);j<i;j++){
+            const currentString = t.slice(j,i)
+            if(strs.includes(currentString)){
+                dp[i] = Math.min(dp[i],dp[j]+1)
             }
         }
     }
-
-    return dp[n] === Infinity ? -1 : dp[n];
+    return dp[n]===Infinity?-1:dp[n]
 }
