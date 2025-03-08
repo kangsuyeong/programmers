f430a81f9cdcbfd43a09bc02aa5b9367371af09c
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-03-08 09:30:52 +0900

    [level 2] Title: 땅따먹기, Time: 65.86 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,11 +1,11 @@
 function solution(land) {
     const n = land.length
-    let dp = Array.from({length:n},()=>Array(4).fill(0))
+    const dp = Array.from({length:n},()=>Array(4).fill(0))
     
+    // dp 첫번째열 셋팅
     for(let i=0;i<4;i++){
         dp[0][i] = land[0][i]
     }
-    
     for(let i=1;i<n;i++){
         for(let j=0;j<4;j++){
             if(j===0){
@@ -19,7 +19,7 @@ function solution(land) {
             }
             else{
                 dp[i][j] = Math.max(dp[i-1][0],dp[i-1][1],dp[i-1][2]) + land[i][j]
-            }            
+            }
         }
     }
     return Math.max(...dp[n-1])
