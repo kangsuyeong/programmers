64a52b663503b96698291fd663bc06be0d70e6d1
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-03-24 14:03:21 +0900

    [level 2] Title: 구명보트, Time: 13.87 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,23 @@
+function solution(people, limit) {
+    let count = 0
+    people.sort((a,b)=>a-b)
+    let left = 0
+    let right = people.length -1
+    
+    while(left<right){
+        if(people[left]+people[right]<=limit){
+            left++
+            right--
+            count++
+        }
+        else{
+            right--
+            count++
+        }
+        
+        if(left===right){
+            count++
+        }
+    }
+    return count
+}
