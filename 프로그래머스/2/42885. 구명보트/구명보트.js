e68bf09a73874a7759a0ee214da7a205bb2be732
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-03-27 08:17:07 +0900

    [level 2] Title: 구명보트, Time: 33.59 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,8 +1,14 @@
 function solution(people, limit) {
-    let count = 0
-    people.sort((a,b)=>a-b)
+    
+    // 필요한 구명보트 수
+    let count =0
+
+    // index
     let left = 0
-    let right = people.length -1
+    let right = people.length-1
+    
+    // 정렬하기
+    people.sort((a,b)=>a-b)
     
     while(left<right){
         if(people[left]+people[right]<=limit){
@@ -14,10 +20,11 @@ function solution(people, limit) {
             right--
             count++
         }
-        
-        if(left===right){
-            count++
-        }
     }
+    
+    if(left===right){
+        count++
+    }
+    
     return count
 }
