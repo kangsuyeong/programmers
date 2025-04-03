639e7c11b78be41bd4b8963586fb9ed7fc63692a
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-04 08:20:56 +0900

    [level 3] Title: 기지국 설치, Time: 3.49 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,31 +1,26 @@
 function solution(n, stations, w) {
-    let total_count =0 // 증설해야하 할 기지국
-    let current_index = 1 // 현재 index
-    let bandWidth = 2*w + 1
+    let result = 0
+    const bandWidth = 2*w + 1
+    let index = 1
     
     stations.sort((a,b)=>a-b)
     
     for(const station of stations){
-        // 현재 기지국에 대한 왼쪽, 오른쪽 index
-        const left = station - w
+        const left = station -w
         const right = station + w
         
-        // 현재 index위치가 left보다 작을경우
-        if(current_index<left){
-            const length = left - current_index // 전파가 도달하지않는 거리
-            const count = Math.ceil(length/bandWidth) // 필요한 기지국 수
-            total_count +=count
+        if(index<left){
+            const length = left - index
+            result += Math.ceil(length / bandWidth)
         }
-        
-        current_index = right+ 1 // 현재 index 업데이트
+   
+        index = right + 1
+    
     }
     
-    // 마지막 기지국부터 끝까지
-    if(current_index<=n){
-        const length = n-current_index + 1
-        const count = Math.ceil(length/bandWidth) // 필요한 기지국 수
-        total_count +=count
+    if(index<=n){
+        const length = n - index + 1
+        result += Math.ceil(length / bandWidth)
     }
-    
-    return total_count
+    return result
 }
