268bf6c17f2f5cccbae12b1482bffea63efed21a
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-04 07:47:24 +0900

    [level 1] Title: 예산, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,14 +1,13 @@
 function solution(d, budget) {
-    // 정렬하기
+    let result = 0
+    let current_money = 0
     d.sort((a,b)=>a-b)
-    let current_money = 0 // 현재 사용한 금액
-    let result=0
     
     for(const money of d){
-        current_money+=money // 현재 사용한 금액 업데이트
-        if(current_money>budget) break
-        result++
+        current_money+=money
+        if(current_money>budget) break;
+        
+        result+=1
     }
-    
     return result
 }
