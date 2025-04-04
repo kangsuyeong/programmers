cb68324cc14ec51560e243ab4ca077d263836f4f
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-04 21:38:48 +0900

    [level 2] Title: 귤 고르기, Time: 126.46 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,17 +1,23 @@
 function solution(k, tangerine) {
-    let result=0
-    let countSum = 0
+    
+    let result = 0 // 귤의 종류
+    let countSum = 0 // 현재 귤의 개수
+    
+    // 귤의 크기별 count
     const count = {}
+    
     for(const t of tangerine){
         count[t] = (count[t]||0) + 1
     }
     
-    const sort_arr = Object.values(count).sort((a,b)=>b-a)
+    // 빈도수에 따라서 내림차순으로 정렬
+    const count_arr = Object.values(count).sort((a,b)=>b-a)
     
-    for(const s of sort_arr){
-        countSum+=s
+    for(const count of count_arr){
+        countSum+=count
         result+=1
         if(countSum>=k) break
     }
     return result
+    
 }
