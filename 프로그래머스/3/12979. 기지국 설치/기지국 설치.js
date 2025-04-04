dc771ddc2eca5c17694a86cadea1047a8446b317
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-04 21:50:04 +0900

    [level 3] Title: 기지국 설치, Time: 3.74 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.js
@@ -1,26 +1,26 @@
 function solution(n, stations, w) {
-    let result = 0
-    const bandWidth = 2*w + 1
-    let index = 1
+    let count = 0 // 증성해야할 기지국 개수
+    const bandWidth = 2*w + 1 
+    let index = 1 // 현재 인덱스
     
+    // 오름차순 정렬
     stations.sort((a,b)=>a-b)
     
     for(const station of stations){
-        const left = station -w
+        const left = station - w
         const right = station + w
         
-        if(index<left){
+        if(index < left){
             const length = left - index
-            result += Math.ceil(length / bandWidth)
+            count += Math.ceil(length / bandWidth)
         }
-   
         index = right + 1
-    
     }
     
     if(index<=n){
-        const length = n - index + 1
-        result += Math.ceil(length / bandWidth)
+        const length = n-index + 1
+        count += Math.ceil(length/bandWidth)
     }
-    return result
+    
+    return count
 }
