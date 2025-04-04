eb4e3b5a023aeb767886595c11d2a5de1825dd77
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-04 21:20:53 +0900

    [level 1] Title: 예산, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,13 +1,15 @@
 function solution(d, budget) {
-    let result = 0
-    let current_money = 0
+    let count = 0 // 지원할 수 있는 부서
+    let current_money = 0 // 현재 사용한 금액
+    
+    // 정렬
     d.sort((a,b)=>a-b)
     
     for(const money of d){
-        current_money+=money
-        if(current_money>budget) break;
+        current_money += money
         
-        result+=1
+        if(current_money>budget) break
+        count+=1
     }
-    return result
+    return count
 }
