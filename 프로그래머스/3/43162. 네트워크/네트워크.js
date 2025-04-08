27f083819865fd82ebea38da1809dbab7edbcb45
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-08 14:12:44 +0900

    [level 3] Title: 네트워크, Time: 27.41 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,24 @@
 function solution(n, computers) {
-    let count = 0 // 네트워크 개수
+    let count =0 // 네트워크 갯수
+    const visited = new Set() // 방문한 노드
     
-    const visited = new Set()
     function dfs(node){
         visited.add(node)
-        for(let i=0;i<n;i++){
-            if(computers[node][i]===1 &&!visited.has(i)){
+        for(let i=0;i<computers.length;i++){
+            if(computers[node][i]===1 && !visited.has(i)){
                 dfs(i)
             }
         }
     }
     
+
+    
     for(let i=0;i<n;i++){
         if(!visited.has(i)){
-            dfs(i)
-            count+=1
+            dfs(i) // dfs로 탐색
+            count+=1 // 네트워크 1 증가
         }
+        console.log(count,visited)
     }
     return count
 }
