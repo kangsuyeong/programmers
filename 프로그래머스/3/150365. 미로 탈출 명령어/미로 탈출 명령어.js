94e9d21c4c72bb138611ac5c58b6d4597d24646b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-11 17:36:38 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 6.29 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,34 +1,37 @@
 function solution(n, m, x, y, r, c, k) {
-    let result = 'impossible';
-    const directions = [[1,0,'d'],[0,-1,'l'],[0,1,'r'],[-1,0,'u']]
+    const directions = [
+        [1,0,'d'],
+        [0,-1,'l'],
+        [0,1,'r'],
+        [-1,0,'u']
+    ]
+    let result = "impossible"
     
     function isRange(x,y){
         return 1<=x && x<=n && 1<=y && y<=m
     }
     
     function dfs(x,y,depth,string){
-        const dist = Math.abs(x - r) + Math.abs(y - c); // 최단거리
-        const remain = k - depth; // 남은거리
+        const dist = Math.abs(x-r) + Math.abs(y-c) // 현재 위치부터 탈출지점 까지 거리
+        const remain = k-depth // 남은 거리
         
-        
-        // ❗ 가지치기 조건 추가
-        if (dist > remain || (remain - dist) % 2 !== 0) return;
+        if(dist>remain || (remain-dist)%2!==0) return
         
         if(depth===k){
             if(x===r && y===c){
-                result=string 
+                result = string
             }
-            return
-        }
-        for(const [dx,dy,word] of directions){
-            const nx = dx + x
-            const ny = dy + y
-            if(isRange(nx,ny)){
-                dfs(nx,ny,depth+1,string+word)
-                if (result !== 'impossible') return; // 이미 정답 찾았으면 종료
-            }
+            return 
         }
         
+        for(const [dx,dy,dir] of directions){
+            const nx = x + dx
+            const ny = y + dy
+            if(isRange(nx,ny)){
+                dfs(nx,ny,depth+1,string+dir)
+                if(result!=="impossible") return
+            }
+        }
     }
     dfs(x,y,0,'')
     return result
