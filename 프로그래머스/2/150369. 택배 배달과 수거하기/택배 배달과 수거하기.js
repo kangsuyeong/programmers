97f25bc604b88dec6b988726555cb973f2b713be
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-11 21:27:19 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 8.09 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,7 +1,7 @@
 function solution(cap, n, deliveries, pickups) {
-    let result = 0 // 최소 이동거리
-    let d = 0 
-    let p = 0
+    let result=0
+    let d=0
+    let p=0
     
     for(let i=n-1;i>=0;i--){
         d+=deliveries[i]
