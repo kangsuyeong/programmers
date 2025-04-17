24046863b1a66bc81c23ddf6c7786c91170c9ca9
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-17 11:21:16 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 426.32 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,28 +1,27 @@
 function solution(arr) {
-    const result=[0,0]
-    function split(x,y,size){
-        const first = arr[x][y]
+    const result =[0,0] // 0 / 1의 갯수
+    
+    function compress(x,y,size){
+        const first = arr[x][y] // 가장 첫번째 숫자
         
-        // if(size===1){
-        //     result[first]+=1
-        //     return
-        // }
-        
+        // 첫번째 문자열과 나머지가 같은지 순회하기
         for(let i=x;i<x+size;i++){
             for(let j=y;j<y+size;j++){
+                // 같지않다면 쪼개기
                 if(first!==arr[i][j]){
                     const half = size/2
-                    split(x,y,half)
-                    split(x+half,y,half)
-                    split(x,y+half,half)
-                    split(x+half,y+half,half)
+                    compress(x,y,half)
+                    compress(x+half,y,half)
+                    compress(x,y+half,half)
+                    compress(x+half,y+half,half)
                     return
                 }
             }
         }
-        
+        // 만약 다같다면 +1 해주기
         result[first]+=1
     }
-    split(0,0,arr.length)
+    
+    compress(0,0,arr.length)
     return result
 }
