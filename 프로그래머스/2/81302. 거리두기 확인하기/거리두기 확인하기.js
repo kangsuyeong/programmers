8a680e8e9c49e3ecf018275ee895eece3509a356
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-21 11:25:08 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,41 +1,47 @@
 function solution(places) {
     const result = []
     for(const place of places){
-        const p = []
+        const people = [] // 사람의 위치
         // p의 위치 확인하기
         for(let i=0;i<5;i++){
             for(let j=0;j<5;j++){
                 if(place[i][j]==='P'){
-                    p.push([i,j])
+                    people.push([i,j])
                 }
             }
         }
-
-        function checkDistance(arr){
-            for(let i=0;i<arr.length-1;i++){
-                for(let j=i+1;j<arr.length;j++){
-                    const [x1,y1] = arr[i]
-                    const [x2,y2] = arr[j]
+        
+        
+        function checkDistance(){
+            for(let i=0;i<people.length-1;i++){
+                for(let j=i+1;j<people.length;j++){
+                    const [x1,y1] = people[i]
+                    const [x2,y2] = people[j]
                     const length = Math.abs(x1-x2) + Math.abs(y1-y2)
-                    if(length<=2){
+                    
+                    // 맨헤튼 거리가 2 이상이면 pass
+                    if(length>2) continue
+                    
+                    if(length===2){
                         // 일자로 파티션 있는 경우
                         // 행이 같고, 그 사이의 가 X값 을 가지는 경우
                         if(x1===x2){
-                            const newY = Math.min(y1,y2) + 1
-                            if(place[x1][newY]==='X') continue
+                            const midY = (y1+y2)/2
+                            if(place[x1][midY]==='X') continue
                         }
                         else if(y1===y2){
-                            const newX = Math.min(x1,x2) + 1
-                            if(place[newX][y1]==='X') continue
+                            const midX = (x1+x2)/2
+                            if(place[midX][y1]==='X') continue
                         }
                         else if(place[x2][y1]==='X' && place[x1][y2]==='X') continue
-                        return 0
                     }
+                    return 0
+                    
                 }
             }
             return 1
         }
-        result.push(checkDistance(p))
+        result.push(checkDistance())
     }
     return result
 }
