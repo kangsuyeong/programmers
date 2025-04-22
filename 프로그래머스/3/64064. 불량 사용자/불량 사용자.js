1bb626f728039bb2fefa52e238aadba62dbc0906
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-22 19:18:14 +0900

    [level 3] Title: 불량 사용자, Time: 101.50 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -0,0 +1,33 @@
+function isMatch(userId, bannedId){
+    if(userId.length!==bannedId.length) return false
+    
+    for(let i=0;i<userId.length;i++){
+        if(bannedId[i]==='*') continue
+        
+        if(userId[i]!==bannedId[i]) return false
+    }
+    return true
+}
+
+function solution(user_id, banned_id) {
+    const result = new Set()
+    function dfs(index,selected){
+        if(index===banned_id.length){
+            const sorted = [...selected].sort().join(',')
+            result.add(sorted)
+            return
+        }
+        
+        for(let i=0;i<user_id.length;i++){
+            if(selected.has(user_id[i])) continue
+                
+            if(isMatch(user_id[i],banned_id[index])){
+                selected.add(user_id[i])
+                dfs(index+1,selected)
+                selected.delete(user_id[i])
+            }
+        }
+    }
+    dfs(0,new Set())
+    return result.size
+}
