6fb21882e1e4509220c4074815f71f7c81ab555f
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-23 11:13:57 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 6.65 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,6 +1,7 @@
 function solution(n, m, x, y, r, c, k) {
-    let result = "impossible"
-    const directions=[
+    let result = ''
+    
+    const directions = [
         [1,0,'d'],
         [0,-1,'l'],
         [0,1,'r'],
@@ -12,31 +13,28 @@ function solution(n, m, x, y, r, c, k) {
     }
     
     function dfs(x,y,depth,string){
-        // 길이가 k일때
+        
+        const dist = Math.abs(x-r) + Math.abs(y-c) // 현재 위치에서 부터 탈출 지점까지 거리
+        const remain = k - depth // 움직을 수 있는 거리
+        
+        if(dist >remain || (remain-dist)%2!==0) return
+        
         if(depth===k){
-            // 탈출했을 때
             if(x===r && y===c){
-                result=string
+                result= string
             }
             return
         }
         
-        const dist = Math.abs(x-r) + Math.abs(y-c) // 현재 위치에서 탈출까지 거리
-        const remain = k-depth // 이동할 수 있는 거리
-        
-        if(dist>remain || (remain-dist)%2!==0) return
-        
         for(const [dx,dy,dir] of directions){
-            const nx = x + dx
+            const nx = x+ dx
             const ny = y + dy
             if(isRange(nx,ny)){
                 dfs(nx,ny,depth+1,string+dir)
-                if(result!=='impossible') return
+                if(result) return
             }
         }
-        
     }
-    
     dfs(x,y,0,'')
-    return result
+    return result ? result : 'impossible'
 }
