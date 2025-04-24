0dd689057c96e7f9f819cfa13a9685c8437452bf
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-24 21:08:18 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -2,7 +2,7 @@ function solution(angle) {
     if(0<angle && angle<90){
         return 1
     }
-    else if(angle===90){
+    else if(angle===  90){
         return 2
     }
     else if(90<angle && angle<180){
