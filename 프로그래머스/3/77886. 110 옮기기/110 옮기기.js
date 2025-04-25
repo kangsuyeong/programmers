9f30b600b27aafa889fa01961317aefb6677e529
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-25 19:57:52 +0900

    [level 3] Title: 110 옮기기, Time: 87.57 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
@@ -2,21 +2,26 @@ function solution(s) {
     const result = []
     
     for(const str of s){
-        const stack = [] // 스택
-        let count = 0 // '110'나오는 횟수
+        const stack = []
+        let count = 0
         for(let i=0;i<str.length;i++){
-            stack.push(str[i])
-            // 스택에 110이 쌓였을때
-            if(stack.length>=3 && stack[stack.length-3]==='1' && stack[stack.length-2]==='1' && stack[stack.length-1]==='0'){
-                stack.splice(-3,3) // 스택에서 110삭제
-                count++ // count 증가
+            const n = stack.length
+            if(n>=2 && stack[n-2]==='1' && stack[n-1]==='1' && str[i]==='0'){
+                stack.pop()
+                stack.pop()
+                count+=1
+                continue
             }
+                
+            stack.push(str[i])
         }
         
-        const rest = stack.join('') // 남아있는 문자열
-        const insertIndex = rest.lastIndexOf('0') + 1 // 삽입해야하는 index -> 가장 마지막 0
+        const rest = stack.join('')
+        const insertIndex = rest.lastIndexOf('0') +1
         const insertString = '110'.repeat(count)
-        result.push(rest.slice(0,insertIndex)+insertString+rest.slice(insertIndex))
+        
+        result.push(rest.slice(0,insertIndex) + insertString + rest.slice(insertIndex))
     }
+    
     return result
 }
