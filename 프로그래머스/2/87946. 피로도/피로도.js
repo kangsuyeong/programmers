c3bfcf0abb94106328b9f666891d8d1bf9205bd7
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-26 19:59:54 +0900

    [level 2] Title: 피로도, Time: 113.22 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,31 +1,35 @@
 function getPermutations(arr,n){
     const result = []
-    if(n===1) return arr.map(v=>[v])
+    if(n===1){
+        return arr.map(v=>[v])
+    }
     
     arr.forEach((fixed,idx,origin)=>{
-        const rest = [...origin.slice(0,idx),...origin.slice(idx+1)] // fixed뺀 나머지 배열
-        const perms = getPermutations(rest,n-1)
-        const attached = perms.map((perm)=>[fixed,...perm])
-        result.push(...attached)
-    })
+        const rest = [...origin.slice(0,idx),...origin.slice(idx+1)] // fixed를 빼고 남은 값 → "나머지"
+        const restPermutations = getPermutations(rest,n-1) // rest로 만든 (n-1)개짜리 순열 → "나머지 순열"
+        const combined = restPermutations.map(p=>[fixed,...p]) //fixed를 앞에 붙인 결과 → "고정+나머지 순열"
+        result.push(...combined)
+    }) 
     return result
 }
 
+
+
 function solution(k, dungeons) {
-    let total_count = 0 // 최대 던전수
-    const allOrders = getPermutations(dungeons, dungeons.length);
-    
-    for(const orders of allOrders){
-        let current_fatigue=k
-        let count = 0 // 탐험한 던전수
-        for(const [need_fatigue,use_fatigue] of orders){
-            if(current_fatigue>=need_fatigue){
-                count+=1
-                current_fatigue-=use_fatigue
-            }
+    let total_count=0
+    const allOrders = getPermutations(dungeons,dungeons.length)
+    for(const order of allOrders){
+        let current_fatigue = k
+        let count = 0
+        for(const [need_fatigue,use_fatigue] of order){
+            if(current_fatigue<need_fatigue) break;
+            
+            count+=1
+            current_fatigue-=use_fatigue
         }
-        
-        total_count = Math.max(total_count,count)
+        if(total_count<count){
+            total_count=count
+        }
     }
     return total_count
 }
