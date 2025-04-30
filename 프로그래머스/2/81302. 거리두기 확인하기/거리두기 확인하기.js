3082fc0c8726c47f86e486e5d166ca682497cccc
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-04-30 17:21:58 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,8 +1,45 @@
+function isValid(people,place){
+    // 조합 확인하기
+    for(let i=0;i<people.length-1;i++){
+        for(let j=i+1;j<people.length;j++){
+            const [x1,y1] = people[i] // 첫번째 사람
+            const [x2,y2] = people[j] // 두번째 사람
+            
+            const distance = Math.abs(x1-x2) + Math.abs(y1-y2)
+            
+            // 거리가 3 이상이면 continue
+            if(distance>=3) continue
+            
+            // 거리가 2미만이면 무조건 거리 지키기 X
+            if(distance<2) return false
+            
+            // 거리가 2일때만 확인
+            
+            // 가로로 위치할 경우
+            if(x1===x2){
+                const midY = (y1+y2)/2
+                if(place[x1][midY]!=='X') return false
+            }
+            else if(y1===y2){
+                const minX = (x1+x2)/2
+                if(place[minX][y1]!=='X') return false
+            }
+            else{
+                if(place[x2][y1]!=='X' || place[x1][y2]!=='X') return false
+            }
+            
+        }
+    }
+    return true
+}
+
 function solution(places) {
-    const result = []
+    const result =[] // 대기실별 결과
+    
     for(const place of places){
-        const people = [] // 사람의 위치
-        // p의 위치 확인하기
+        const people = [] // 현재 사람이 있는 좌표
+        
+        // 사람의 위치 확인하기
         for(let i=0;i<5;i++){
             for(let j=0;j<5;j++){
                 if(place[i][j]==='P'){
@@ -11,37 +48,14 @@ function solution(places) {
             }
         }
         
-        
-        function checkDistance(){
-            for(let i=0;i<people.length-1;i++){
-                for(let j=i+1;j<people.length;j++){
-                    const [x1,y1] = people[i]
-                    const [x2,y2] = people[j]
-                    const length = Math.abs(x1-x2) + Math.abs(y1-y2)
-                    
-                    // 맨헤튼 거리가 2 이상이면 pass
-                    if(length>2) continue
-                    
-                    if(length===2){
-                        // 일자로 파티션 있는 경우
-                        // 행이 같고, 그 사이의 가 X값 을 가지는 경우
-                        if(x1===x2){
-                            const midY = (y1+y2)/2
-                            if(place[x1][midY]==='X') continue
-                        }
-                        else if(y1===y2){
-                            const midX = (x1+x2)/2
-                            if(place[midX][y1]==='X') continue
-                        }
-                        else if(place[x2][y1]==='X' && place[x1][y2]==='X') continue
-                    }
-                    return 0
-                    
-                }
-            }
-            return 1
+        if(isValid(people,place)){
+            result.push(1)
         }
-        result.push(checkDistance())
+        else{
+            result.push(0)
+        }
+        
+
     }
     return result
 }
