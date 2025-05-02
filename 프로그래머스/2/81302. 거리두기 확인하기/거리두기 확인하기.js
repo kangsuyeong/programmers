09f9b8bf5797495bf3cb4a8efe42a1b0c2c91a01
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-02 21:12:01 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,45 +1,44 @@
 function isValid(people,place){
-    // 조합 확인하기
     for(let i=0;i<people.length-1;i++){
         for(let j=i+1;j<people.length;j++){
-            const [x1,y1] = people[i] // 첫번째 사람
-            const [x2,y2] = people[j] // 두번째 사람
+            const [x1,y1] = people[i] // 사람 1의 좌표
+            const [x2,y2] = people[j] // 사람 2의 좌표
+
+            const distance = Math.abs(x1-x2) + Math.abs(y1-y2) // 거리
+
+            if(distance===1) return false 
+            if(distance>2) continue
             
-            const distance = Math.abs(x1-x2) + Math.abs(y1-y2)
+            //거리가 2일 경우
             
-            // 거리가 3 이상이면 continue
-            if(distance>=3) continue
-            
-            // 거리가 2미만이면 무조건 거리 지키기 X
-            if(distance<2) return false
-            
-            // 거리가 2일때만 확인
-            
-            // 가로로 위치할 경우
+            // 가로로 있을경우
             if(x1===x2){
                 const midY = (y1+y2)/2
                 if(place[x1][midY]!=='X') return false
             }
+            // 세로로 있을 경우
             else if(y1===y2){
-                const minX = (x1+x2)/2
-                if(place[minX][y1]!=='X') return false
-            }
-            else{
-                if(place[x2][y1]!=='X' || place[x1][y2]!=='X') return false
+                const midX = (x1+x2)/2
+                if(place[midX][y1]!=='X') return false
             }
             
+            // 대각선일 경우
+            else{
+                if(place[x1][y2]!=='X' || place[x2][y1]!=='X') return false
+            }
+
         }
     }
     return true
 }
 
 function solution(places) {
-    const result =[] // 대기실별 결과
+    const result = []
     
     for(const place of places){
-        const people = [] // 현재 사람이 있는 좌표
+        const people = [] // 사람의 좌표 저장
         
-        // 사람의 위치 확인하기
+        // 사람 좌표 찾기
         for(let i=0;i<5;i++){
             for(let j=0;j<5;j++){
                 if(place[i][j]==='P'){
@@ -47,15 +46,15 @@ function solution(places) {
                 }
             }
         }
+        // console.log(people)
         
+        // 각각의 거리 계산하기
         if(isValid(people,place)){
             result.push(1)
         }
         else{
             result.push(0)
         }
-        
-
     }
     return result
 }
