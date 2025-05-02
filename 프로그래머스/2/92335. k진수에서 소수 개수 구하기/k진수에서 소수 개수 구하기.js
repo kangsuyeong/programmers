6914ac9969fa347125be09abefc6a2d32343438c
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-02 20:48:37 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 8.40 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,25 +1,24 @@
 function isPrime(n){
-    if(n===1) return false
+    if(n<=1) return false
     if(n===2) return true
     if(n%2===0) return false
-    for(let i=3;i<=Math.sqrt(n);i++){
+    for(let i=3;i<=Math.sqrt(n);i+=2){
         if(n%i===0) return false
     }
     return true
 }
 
 function solution(n, k) {
-    let result = 0 // 소수의 개수
-    
+    let result = 0 // 소수 갯수
     const num_arr = n.toString(k).split('0')
+    // console.log(num_arr)
     
     for(const num of num_arr){
-        if(num==='') continue // 빈문자열일 경우 패스
+        if(num==='') continue
         
         if(isPrime(Number(num))){
             result+=1
         }
-        
     }
     return result
 }
