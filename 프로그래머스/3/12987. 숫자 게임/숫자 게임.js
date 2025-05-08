23ee087316a8ebd7e6ef50dde4438b2b52e90a59
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-08 19:32:22 +0900

    [level 3] Title: 숫자 게임, Time: 85.92 ms, Memory: 44.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -0,0 +1,15 @@
+function solution(A, B) {
+    A.sort((a,b)=>a-b)
+    B.sort((a,b)=>a-b)
+    
+    let score = 0
+    let index = 0 // A의 비교대상
+    
+    for(let i=0;i<B.length;i++){
+        if(B[i]>A[index]){
+            score+=1
+            index+=1
+        }
+    }
+    return score
+}
