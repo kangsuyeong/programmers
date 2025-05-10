d95707539ebd04c99c49510fd23b185d91184f79
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-10 11:35:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 397.54 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -2,9 +2,11 @@ class Queue{
     items=[]
     rear=0
     front=0
+    
     constructor(arr){
-        this.items=arr
+        this.items = arr
     }
+    
     push(item){
         this.items.push(item)
         this.rear++
@@ -12,37 +14,39 @@ class Queue{
     pop(){
         return this.items[this.front++]
     }
+    size(){
+        return this.rear-this.front
+    }
 }
 
 function solution(queue1, queue2) {
     const n = queue1.length
+    
     let q1_sum = queue1.reduce((acc,cur)=>acc+cur,0)
     let q2_sum = queue2.reduce((acc,cur)=>acc+cur,0)
+    const total_sum = q1_sum+q2_sum
+    if(total_sum%2!==0) return -1
+    const target = total_sum / 2
+    
+    
     const q1 = new Queue(queue1)
     const q2 = new Queue(queue2)
     
-    const total = q1_sum + q2_sum
-    const target = total/2
-    let result = 0
-    
-    if(total%2!==0) return -1
-    
-    while(result<n*4){
-        if(q1_sum===q2_sum) return result
+    for(let i=0;i<n*3;i++){
+        if(q1_sum===q2_sum) return i
         
         if(q1_sum>q2_sum){
             const item = q1.pop()
-            q2.push(item)
             q1_sum -=item
             q2_sum+=item
+            q2.push(item)
         }
         else{
             const item = q2.pop()
+            q2_sum -= item
+            q1_sum += item
             q1.push(item)
-            q1_sum+=item
-            q2_sum-=item
         }
-        result+=1
     }
     return -1
 }
