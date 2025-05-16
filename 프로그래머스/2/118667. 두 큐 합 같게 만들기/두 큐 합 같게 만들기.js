4496e4f7192bc8678cee6a3aa264f69ac51437d2
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-16 21:15:13 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 28.74 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -2,11 +2,9 @@ class Queue{
     items=[]
     rear=0
     front=0
-    
     constructor(arr){
         this.items = arr
     }
-    
     push(item){
         this.items.push(item)
         this.rear++
@@ -14,20 +12,16 @@ class Queue{
     pop(){
         return this.items[this.front++]
     }
-    size(){
-        return this.rear-this.front
-    }
 }
 
 function solution(queue1, queue2) {
     const n = queue1.length
-    
     let q1_sum = queue1.reduce((acc,cur)=>acc+cur,0)
     let q2_sum = queue2.reduce((acc,cur)=>acc+cur,0)
-    const total_sum = q1_sum+q2_sum
-    if(total_sum%2!==0) return -1
-    const target = total_sum / 2
     
+    const total_sum = q1_sum + q2_sum
+    if(total_sum%2!==0) return -1
+    const target = total_sum / 2 // 모든 큐의 원소의 합의 반
     
     const q1 = new Queue(queue1)
     const q2 = new Queue(queue2)
@@ -37,14 +31,14 @@ function solution(queue1, queue2) {
         
         if(q1_sum>q2_sum){
             const item = q1.pop()
-            q1_sum -=item
+            q1_sum-=item
             q2_sum+=item
             q2.push(item)
         }
         else{
             const item = q2.pop()
-            q2_sum -= item
-            q1_sum += item
+            q2_sum -=item
+            q1_sum +=item
             q1.push(item)
         }
     }
