564cf24def0f215aabe160cfd856be44f6af74ae
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-16 22:15:53 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 40.80 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
@@ -1,13 +1,13 @@
 function solution(alp, cop, problems) {
-    
     let max_alp = 0
     let max_cop = 0
     
-    for(const [alp,cop] of problems){
-        max_alp = Math.max(max_alp,alp)
-        max_cop = Math.max(max_cop,cop)
+    // 최대 알고력, 코딩력 구하기
+    for(const [alp_req,cop_req] of problems){
+        max_alp = Math.max(alp_req,max_alp)
+        max_cop = Math.max(cop_req,max_cop)
     }
-
+    
     const dp = Array.from({length:max_alp+2},()=>Array(max_cop+2).fill(Infinity))
     alp = Math.min(alp,max_alp)
     cop = Math.min(cop,max_cop)
@@ -16,8 +16,8 @@ function solution(alp, cop, problems) {
     for(let i=alp;i<=max_alp;i++){
         for(let j=cop;j<=max_cop;j++){
             dp[i+1][j] = Math.min(dp[i+1][j],dp[i][j]+1)
-            dp[i][j+1] = Math.min(dp[i][j+1],dp[i][j] + 1)
-            
+            dp[i][j+1] = Math.min(dp[i][j+1],dp[i][j]+1)
+
             for(const [alp_req,cop_req,alp_rwd,cop_rwd,cost] of problems){
                 if(i>=alp_req && j>=cop_req){
                     const next_alp = Math.min(max_alp,i+alp_rwd)
@@ -27,5 +27,5 @@ function solution(alp, cop, problems) {
             }
         }
     }
-    return dp[max_alp][max_cop]
+    return dp[max_alp][max_cop ]
 }
