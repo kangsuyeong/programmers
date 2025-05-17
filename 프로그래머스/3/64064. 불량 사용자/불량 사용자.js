9b0f4c7c64373acbf94b24eeb15c78c0a98663d3
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-17 16:40:05 +0900

    [level 3] Title: 불량 사용자, Time: 75.89 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,17 +1,16 @@
-function isMatch(userId,bannedId){
-    if(userId.length!==bannedId.length) return false
+function isMatch(u_id,b_id){
+    if(u_id.length!==b_id.length) return false
     
-    for(let i=0;i<userId.length;i++){
-        if(bannedId[i]==='*') continue
+    for(let i=0;i<u_id.length;i++){
+        if(b_id[i]==='*') continue
         
-        if(userId[i]!==bannedId[i]) return false
+        if(u_id[i]!==b_id[i]) return false
     }
-    
     return true
 }
 
 function solution(user_id, banned_id) {
-    const result = new Set()
+    let result = new Set() // 가능한 목록
     
     function dfs(index,selected){
         if(index===banned_id.length){
@@ -22,14 +21,14 @@ function solution(user_id, banned_id) {
         
         for(let i=0;i<user_id.length;i++){
             if(selected.has(user_id[i])) continue
-            if(!isMatch(user_id[i],banned_id[index])) continue
             
-            selected.add(user_id[i])
-            dfs(index+1,selected)
-            selected.delete(user_id[i])
+            if(isMatch(user_id[i],banned_id[index])){
+                selected.add(user_id[i])
+                dfs(index+1,selected)
+                selected.delete(user_id[i])
+            }
         }
     }
-    
     dfs(0,new Set())
     return result.size
 }
