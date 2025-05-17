c92babb54d57285ba4ddbe589573c5cb2005dc6b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-17 16:49:41 +0900

    [level 2] Title: 가장 큰 수, Time: 120.69 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,10 +1,11 @@
 function solution(numbers) {
-    numbers.sort((a,b)=>{
-        // 음수일때 자리를 안바꾼다.
-        if(String(a)+String(b) >String(b)+String(a)){
-            return -1
-        }
-        return 1
-    })
-    return numbers.join('')[0] ==='0' ? '0' : numbers.join('')
+    let result = numbers.sort((a,b)=>{
+        if(String(a)+String(b)>String(b)+String(a)) return -1
+        else return 1
+    }).join('')
+    
+    
+    if(result[0]==='0') return '0'
+    
+    return result 
 }
