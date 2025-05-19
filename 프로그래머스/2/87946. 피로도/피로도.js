9c2beca17c6f3d6411c4d6af7668f13a6ffff066
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-19 21:15:59 +0900

    [level 2] Title: 피로도, Time: 93.25 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,35 +1,29 @@
-function getPermutations(arr,n){
+function getPermutation(arr,n){
     const result = []
-    if(n===1){
-        return arr.map(v=>[v])
-    }
+    if(n===1) return arr.map(v=>[v])
     
-    arr.forEach((fixed,idx,origin)=>{
-        const rest = [...origin.slice(0,idx),...origin.slice(idx+1)] // fixed를 빼고 남은 값 → "나머지"
-        const restPermutations = getPermutations(rest,n-1) // rest로 만든 (n-1)개짜리 순열 → "나머지 순열"
-        const combined = restPermutations.map(p=>[fixed,...p]) //fixed를 앞에 붙인 결과 → "고정+나머지 순열"
+    arr.forEach((fixed,index,origin)=>{
+        const rest = [...origin.slice(0,index),...origin.slice(index+1)]
+        const restPermutation = getPermutation(rest,n-1)
+        const combined = restPermutation.map(p=>[fixed,...p])
         result.push(...combined)
-    }) 
+    })
     return result
 }
 
-
-
 function solution(k, dungeons) {
-    let total_count=0
-    const allOrders = getPermutations(dungeons,dungeons.length)
-    for(const order of allOrders){
-        let current_fatigue = k
+    let total = 0
+    const perms = getPermutation(dungeons,dungeons.length)
+    for(const perm of perms){
+        let cur = k
         let count = 0
-        for(const [need_fatigue,use_fatigue] of order){
-            if(current_fatigue<need_fatigue) break;
-            
-            count+=1
-            current_fatigue-=use_fatigue
+        for(const [need,use] of perm){
+            if(cur>=need){
+                count+=1
+                cur-=use
+            }
         }
-        if(total_count<count){
-            total_count=count
-        }
+        total = Math.max(total,count)
     }
-    return total_count
+    return total
 }
