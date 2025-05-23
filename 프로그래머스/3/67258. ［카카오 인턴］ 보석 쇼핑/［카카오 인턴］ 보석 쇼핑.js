c87926b411e89ef4e5c7088f95b6481652d899a5
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-23 21:50:38 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 43.15 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -5,28 +5,21 @@ function solution(gems) {
     let min_length = gems.length
     let result = [1,gems.length]
     
-    
     for(let right=0;right<gems.length;right++){
+        const gemRight = gems[right]
+        gemsMap.set(gemRight,(gemsMap.get(gemRight)||0) + 1)
         
-        const gem = gems[right]
-        gemsMap.set(gem,(gemsMap.get(gem)||0)+1)
-        
-        // 모든 종류의 보석을 가지고있을 때
         while(gemsKinds===gemsMap.size){
-            
-            // 최소길이 index 업데이트
-            const current_length = right-left + 1
-            if(current_length<min_length){
-                min_length = current_length
+            if(min_length>right - left + 1){
+                min_length = right - left + 1
                 result = [left+1,right+1]
             }
             
-            // 왼쪽 인덱스 제거하기
-            const leftGem = gems[left]
-            gemsMap.set(leftGem,gemsMap.get(leftGem)-1)
+            const gemleft = gems[left]
+            gemsMap.set(gemleft,gemsMap.get(gemleft) - 1)
             
-            if(gemsMap.get(leftGem)===0){
-                gemsMap.delete(leftGem)
+            if(gemsMap.get(gemleft)===0){
+                gemsMap.delete(gemleft)
             }
             left++
         }
