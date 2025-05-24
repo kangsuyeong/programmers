a457897bd2b59d4cc6e3872f18644c194436f068
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-24 16:40:29 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 37.76 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -4,22 +4,21 @@ function solution(gems) {
     const gemsMap = new Map()
     let left = 0
     let right = 0
-    let result = [1,n]
     let min_length = n
+    let result = [1,n]
     
     while(right<n){
         const gemsRight = gems[right]
         gemsMap.set(gemsRight,(gemsMap.get(gemsRight)||0)+1)
         
         while(gemsMap.size===gemsKinds){
-            const current_length = right-left + 1
-            if(min_length>current_length){
-                min_length = current_length
-                result=[left+1,right+1]
+            const cur_length = right - left +1
+            if(cur_length<min_length){
+                min_length = cur_length
+                result = [left+1,right+1]
             }
             
             const gemsLeft = gems[left]
-            
             gemsMap.set(gemsLeft,gemsMap.get(gemsLeft)-1)
             
             if(gemsMap.get(gemsLeft)===0){
