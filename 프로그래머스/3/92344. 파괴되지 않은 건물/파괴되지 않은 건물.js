e3de7d591b908925720a97baa9efd2fb9c34af4b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-24 15:12:08 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 115.95 ms, Memory: 183 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -5,39 +5,39 @@ function solution(board, skill) {
     const psum = Array.from({length:n+1},()=>Array(m+1).fill(0))
     
     for(const s of skill){
-        const [type,r1,c1,r2,c2,degree] = s
+        const [type, r1, c1, r2, c2, degree] = s
         
         const value = type===1 ? -degree : degree
         
         psum[r1][c1] += value
-        psum[r1][c2+1] -= value
-        psum[r2+1][c1] -= value
+        psum[r1][c2+1] -=value
+        psum[r2+1][c1] -=value
         psum[r2+1][c2+1] +=value
     }
     
-    for(let i=0;i<n+1;i++){
-        for(let j=1;j<m+1;j++){
+    for(let i=0;i<=n;i++){
+        for(let j=1;j<=n;j++){
             psum[i][j] += psum[i][j-1]
         }
     }
     
-    for(let j=0;j<m+1;j++){
-        for(let i=1;i<n+1;i++){
+    for(let j=0;j<=m;j++){
+        for(let i=1;i<=n;i++){
             psum[i][j] += psum[i-1][j]
         }
     }
     
     for(let i=0;i<n;i++){
         for(let j=0;j<m;j++){
-            board[i][j] += psum[i][j]
+            board[i][j] +=psum[i][j]
         }
     }
     
-    let count=0
+    let count =0
     for(let i=0;i<n;i++){
         for(let j=0;j<m;j++){
             if(board[i][j]>0){
-                count+=1
+                count++
             }
         }
     }
