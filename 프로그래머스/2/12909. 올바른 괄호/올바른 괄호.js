8325c866b3fd59f8e6cdc05435b075b604fbb574
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-05-27 20:48:11 +0900

    [level 2] Title: 올바른 괄호, Time: 31.86 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,20 @@
+function solution(s){
+    if(s.length%2!==0) return false
+    
+    const stack = []
+    
+    for(const string of s){
+        if(string==='('){
+            stack.push(string)
+        }
+        else{
+            if(stack.length===0) return false
+            const item = stack.pop()
+            if(item===')') return false
+        }
+    }
+    
+    if(stack.length>0) return false
+    
+    return true
+}
