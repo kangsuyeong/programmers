fc28cbb56466a27195d2889415b35edbb6dd0cbd
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-06-07 20:55:46 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 16.53 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,27 +1,47 @@
+class Queue{
+    items=[]
+    rear=0
+    front=0
+    constructor(array){
+        this.items=array
+        this.rear = array.length
+    }
+    push(item){
+        this.items.push(item)
+        this.rear++
+    }
+    pop(){
+        return this.items[this.front++]
+    }
+    size(){
+        return this.rear - this.front
+    }
+    first(){
+        return this.items[this.front]
+    }
+}
+
 function solution(bridge_length, weight, truck_weights) {
+    const bridge = new Queue(Array(bridge_length).fill(0))
+    truck_weights = new Queue(truck_weights)
+    let current_weight = 0
     let time = 0
-    const bridge = Array(bridge_length).fill(0) // 다리
-    let current_weigth = 0 // 현재 무게
     
-    // 다리에 아무것도 없을때 까지
-    while(bridge.length>0){
-        const bridge_item = bridge.shift()
-        current_weigth-=bridge_item
+    while(bridge.size()>0){
+        //먼저 다리에 있는거 하나 빼기
+        const bridge_item = bridge.pop()
+        current_weight-=bridge_item
         
-        
-        if(truck_weights.length>0){
-            // 대기 트럭 첫번째 + 현재 무게 
-            if(truck_weights[0] + current_weigth<=weight){
-                const truck_item = truck_weights.shift()
+        if(truck_weights.size()>0){
+            if(truck_weights.first() + current_weight <=weight){
+                const truck_item = truck_weights.pop()
                 bridge.push(truck_item)
-                current_weigth+=truck_item // 현재 무게 업데이트
+                current_weight+=truck_item
             }
             else{
                 bridge.push(0)
-            } 
+            }
         }
-        
-
         time++
     }
     return time
