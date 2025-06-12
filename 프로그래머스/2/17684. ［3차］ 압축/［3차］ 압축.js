316d4a7a16639057489993dfdf0c55b975622ddf
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-06-12 16:09:51 +0900

    [level 2] Title: [3차] 압축, Time: 0.69 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,28 +1,30 @@
 function solution(msg) {
-    const result = []
-    const letterMap = new Map()
-    
+    const dict = new Map()
     for(let i=0;i<26;i++){
-        letterMap.set(String.fromCharCode(65+i),i+1)
+        dict.set(String.fromCharCode(65+i),i+1)
     }
     
-    let w = ''
+    const result = []
+    
+    let w = '' // 사전에서 현재 입력과 일치하는 가장 긴 문자열
     for(let i=0;i<msg.length;i++){
-        const c = msg[i]
-        const wc = w+c
-        if(letterMap.has(wc)){
+        const c = msg[i] // 다음 문자열
+        const wc = w+c // 사전에서 현재 입력과 일치하는 가장 긴 문자열 + 다음 문자열
+        
+        // wc가 있는지 확인
+        if(dict.has(wc)){
             w=wc
         }
         else{
-            result.push(letterMap.get(w))
-            letterMap.set(wc,letterMap.size+1)
+            result.push(dict.get(w)) // w에 해당하는 index번호 push
+            dict.set(wc,dict.size+1) // 사전 업데이트
             w=c
+            
         }
-        
-        
     }
     
-    if(w) result.push(letterMap.get(w))
+    // 마지막 w가 남아있으면
+    if(w)  result.push(dict.get(w))
     
-    return result
+   return result
 }
