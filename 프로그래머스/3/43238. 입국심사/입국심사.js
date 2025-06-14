e5a073aeb6c67db3abf0acf8d95f57227e2201e0
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-06-14 12:21:27 +0900

    [level 3] Title: 입국심사, Time: 115.19 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,15 +1,13 @@
 function solution(n, times) {
-    let left = 1 // 최소 걸리는 시간
-    let right = Math.max(...times)*n // 최대 걸리는 시간
+    let left = 1
+    let right = Math.max(...times) * n
     let result = right
-
+    
     while(left<=right){
-        const mid = Math.floor((left+right)/2) // 가운데 시간
+        const mid = Math.floor((left+right)/2)
         
-        // 심시관 당 시간동안 할수있는 인원 계산 후 합치기
         const total = times.reduce((acc,cur)=>acc+Math.floor(mid/cur),0)
         
-        // 현재 심사할수있는 인원이 많은경우
         if(total>=n){
             result = mid
             right = mid-1
