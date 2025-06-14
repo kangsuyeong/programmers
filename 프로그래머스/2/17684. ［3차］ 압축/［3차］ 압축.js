9aac631265f48ea8d48d7bed72aeb4bcc63fb50a
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-06-14 13:21:50 +0900

    [level 2] Title: [3차] 압축, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -5,24 +5,24 @@ function solution(msg) {
         dict.set(String.fromCharCode(65+i),i+1)
     }
     
-    let w = '' // 가장 긴 문자열
+    let w = '' // 현재 가장 긴 문자열
     const result = []
+    
     for(let i=0;i<msg.length;i++){
         const c = msg[i] // 현재 문자열
         const wc = w+c
         
-        if(dict.has(wc)){
-            w=wc // 가장 긴 문자열 업데이트
+        if(dict.get(wc)){
+            w = wc
         }
         else{
-            result.push(dict.get(w)) // result에 푸시
-            dict.set(wc,dict.size+1) // 사전 업데이트
-            w=c // 가장 긴 문자열 업데이트
+            result.push(dict.get(w))
+            dict.set(wc,dict.size+1)
+            w=c
         }
     }
     
     result.push(dict.get(w))
-    
     
     return result
 }
