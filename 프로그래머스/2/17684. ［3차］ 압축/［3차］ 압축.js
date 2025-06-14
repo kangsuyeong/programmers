9390922ec84383657fd67630c2fd10342d2d692b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-06-14 12:16:20 +0900

    [level 2] Title: [3차] 압축, Time: 0.39 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -1,30 +1,28 @@
 function solution(msg) {
     const dict = new Map()
+    
     for(let i=0;i<26;i++){
         dict.set(String.fromCharCode(65+i),i+1)
     }
     
+    let w = '' // 가장 긴 문자열
     const result = []
-    
-    let w = '' // 사전에서 현재 입력과 일치하는 가장 긴 문자열
     for(let i=0;i<msg.length;i++){
-        const c = msg[i] // 다음 문자열
-        const wc = w+c // 사전에서 현재 입력과 일치하는 가장 긴 문자열 + 다음 문자열
+        const c = msg[i] // 현재 문자열
+        const wc = w+c
         
-        // wc가 있는지 확인
         if(dict.has(wc)){
-            w=wc
+            w=wc // 가장 긴 문자열 업데이트
         }
         else{
-            result.push(dict.get(w)) // w에 해당하는 index번호 push
+            result.push(dict.get(w)) // result에 푸시
             dict.set(wc,dict.size+1) // 사전 업데이트
-            w=c
-            
+            w=c // 가장 긴 문자열 업데이트
         }
     }
     
-    // 마지막 w가 남아있으면
-    if(w)  result.push(dict.get(w))
+    result.push(dict.get(w))
     
-   return result
+    
+    return result
 }
