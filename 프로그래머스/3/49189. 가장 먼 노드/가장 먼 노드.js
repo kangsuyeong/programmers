37b4c756d10c9bae3173cd9d203d31edbb5df869
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-06-29 13:22:55 +0900

    [level 3] Title: 가장 먼 노드, Time: 61.09 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -2,7 +2,6 @@ class Queue{
     items=[]
     rear=0
     front=0
-    
     push(item){
         this.items.push(item)
         this.rear++
@@ -15,10 +14,9 @@ class Queue{
     }
 }
 
-
 function solution(n, edge) {
-    // 인접 리스트 만들기
     const adjList = {}
+    
     for(const [n1,n2] of edge){
         adjList[n1] = adjList[n1] || []
         adjList[n2] = adjList[n2] || []
@@ -27,22 +25,22 @@ function solution(n, edge) {
         adjList[n2].push(n1)
     }
     
-    // 거리를 저장하는 배열
     const distance = Array(n+1).fill(-1)
-    const q = new Queue()
-    q.push([1,0]) // [node,dist]
+    
+    const queue = new Queue()
+    queue.push([1,0]) // [node,dist]
     distance[1] = 0
     
-    while(q.size()>0){
-        const [node,dist] = q.pop()
-        for(const neighbor of adjList[node]){
-            if(distance[neighbor]>=0) continue
+    while(queue.size()>0){
+        const [node,dist] = queue.pop()
+        
+        for(const next of adjList[node]){
+            if(distance[next]>=0) continue
             
-            q.push([neighbor,dist+1])
-            distance[neighbor] = dist+1
+            distance[next] = dist+1
+            queue.push([next,dist+1])
         }
     }
-    
     const maxNum = Math.max(...distance)
     const count = distance.filter(d=>d===maxNum).length
     return count
