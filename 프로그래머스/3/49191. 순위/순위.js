a6005e755df6bf67508962a3c91ef05585d2ba59
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-06-29 13:39:34 +0900

    [level 3] Title: 순위, Time: 11.44 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,33 +1,35 @@
+function dfs(graph,node,visited){
+    for(const next of graph[node] || []){
+        if(!visited.has(next)){
+            visited.add(next)
+            dfs(graph,next,visited)
+        }
+    }
+}
+
 function solution(n, results) {
-    // 경기 결과를 계산하는 배열
-    const graph = Array.from({length:n+1},()=>Array(n+1).fill(0))
+    const win = {}
+    const lose = {}
     
     for(const [winner,loser] of results){
-        graph[winner][loser] = 1 // 승리
-        graph[loser][winner] = -1 // 패배
-    }
-    
-    // 플로이드 알고리즘
-    for(let k=1;k<=n;k++){ // 경유지
-        for(let i=1;i<=n;i++){ // 본인
-            for(let j=1;j<=n;j++){ // 상대방
-                if(graph[i][k]===1 && graph[k][j]===1){
-                    graph[i][j]=1
-                }
-                if(graph[i][k]===-1 && graph[k][j]===-1){
-                    graph[i][j]=-1
-                }
-            } 
-        }
+        win[winner] = win[winner] || []
+        lose[loser] = lose[loser] || []
+        
+        win[winner].push(loser)
+        lose[loser].push(winner)
     }
     
     let result = 0
+    
     for(let i=1;i<=n;i++){
-        let known = 0
-        for(let j=1;j<=n;j++){
-            if(i!==j && graph[i][j]!==0) known++
-        }
-        if(known===n-1) result++
+        const winSet = new Set()
+        const loseSet = new Set()
+        
+        dfs(win,i,winSet)
+        dfs(lose,i,loseSet)
+        
+        const total = winSet.size + loseSet.size
+        if(total==n-1) result++
     }
     return result
 }
