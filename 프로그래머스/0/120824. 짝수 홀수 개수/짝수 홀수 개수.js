6ebbf990ae7fe68b54d25441c6d59029fe9b0c95
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-14 20:53:52 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,7 +1,7 @@
 function solution(num_list) {
     let even = 0
     let odd =0
-    for(const num of num_list){
+    for(const num of num_list  ){
         if (num%2===0)
             even+=1;
         else
