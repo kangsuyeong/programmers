7c5684ca0f15a464fff63c26093d211d8dbfc410
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-19 13:43:15 +0900

    [level 3] Title: 합승 택시 요금, Time: 91.09 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,25 +1,25 @@
 function solution(n, s, a, b, fares) {
     const distance = Array.from({length:n+1},()=>Array(n+1).fill(Infinity))
-    let result = Infinity
     
-    for(let i=1;i<=n;i++) distance[i][i]=0
+    for(let i=1;i<=n;i++) distance[i][i] = 0
     
     for(const fare of fares){
-        const [start,end,dist] = fare
-        distance[start][end] = dist
-        distance[end][start] = dist
+        const[start,end,cost] = fare
+        distance[start][end] = cost
+        distance[end][start] = cost
     }
     
     for(let k=1;k<=n;k++){
         for(let i=1;i<=n;i++){
             for(let j=1;j<=n;j++){
-                distance[i][j] = Math.min(distance[i][j],distance[i][k] + distance[k][j])
+                distance[i][j] = Math.min(distance[i][k] + distance[k][j],distance[i][j])
             }
         }
     }
     
+    let result = Infinity
     for(let k=1;k<=n;k++){
         result = Math.min(result,distance[s][k] + distance[k][a] + distance[k][b])
     }
-    return result 
+    return result
 }
