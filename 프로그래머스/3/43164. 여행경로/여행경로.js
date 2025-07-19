ef8b00370406eee87254aeae91677ef63c8294c9
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-19 14:00:02 +0900

    [level 3] Title: 여행경로, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,11 +1,12 @@
 function solution(tickets) {
+    // 알파벳 순서대로 정렬
     tickets.sort()
     
-    const visited = Array(tickets.length).fill(false)
     let result = []
+    const visited = Array(tickets.length).fill(false)
     let found = false
     
-    function dfs(depth,path,current){
+    function dfs(current,path,depth){
         if(found) return
         
         if(depth===tickets.length){
@@ -17,14 +18,14 @@ function solution(tickets) {
         for(let i=0;i<tickets.length;i++){
             const [start,end] = tickets[i]
             if(!visited[i] && current===start){
-                visited[i]=true
-                dfs(depth+1,[...path,end],end)
-                visited[i]=false
+                visited[i] = true
+                dfs(end,[...path,end],depth+1)
+                visited[i] = false
             }
         }
     }
     
-    dfs( 0,["ICN"],"ICN");
+    dfs('ICN',['ICN'],0)
     
     return result
 }
