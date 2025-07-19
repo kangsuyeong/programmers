3673941cbe31537ad51b7d5c646472e95c9ce56b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-19 13:29:55 +0900

    [level 2] Title: 타겟 넘버, Time: 13.77 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,14 +1,13 @@
-
 function solution(numbers, target) {
     let result = 0
     
     function dfs(index,total){
         if(index===numbers.length){
-            if(total===target){
-                result++
-            }
+            if(total===target) result++
+            
             return
         }
+        
         dfs(index+1,total+numbers[index])
         dfs(index+1,total-numbers[index])
     }
