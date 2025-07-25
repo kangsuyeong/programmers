7279c984371030a2325eb8d2a011105b23355938
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-25 21:00:50 +0900

    [level 5] Title: 방의 개수, Time: 493.43 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.js
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -1,4 +1,37 @@
 function solution(arrows) {
-    var answer = 0;
-    return answer;
+    let result = 0
+    const dx = [0, 1, 1, 1, 0, -1, -1, -1];
+    const dy = [1, 1, 0, -1, -1, -1, 0, 1];
+    
+    const visitedNodes = new Set()
+    const visitedEdges = new Set()
+    
+    let x = 0
+    let y = 0
+    
+    visitedNodes.add(`0,0`)
+    
+    for(const dir of arrows){
+        for(let i=0;i<2;i++){
+            const nx = x + dx[dir]
+            const ny = y + dy[dir]
+            
+            const curNode = `${x},${y}`
+            const nextNode = `${nx},${ny}`
+            const edge = `${curNode}-${nextNode}`
+            const reverseEdge = `${nextNode}-${curNode}`
+            
+            // 다음 노드가 이미 방문했고, 처음가는 길일 경우 -> 방생성
+            if(visitedNodes.has(nextNode) && !visitedEdges.has(edge)){
+                result++
+            }
+            visitedNodes.add(nextNode)
+            visitedEdges.add(edge)
+            visitedEdges.add(reverseEdge)
+            
+            x = nx
+            y = ny
+        }
+    }
+    return result
 }
