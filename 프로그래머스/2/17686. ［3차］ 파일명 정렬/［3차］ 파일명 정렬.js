e1ec22842af5bd792be02a6571f77f0e09ec0741
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-26 13:39:08 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 31.72 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,16 +1,14 @@
 function splitFile(file){
-    let head = ''
-    let number = ''
-    
+    let head=''
+    let number=''
     let index = 0
     
-    // head 계산하기 -> 숫자가 나올때까지 계산
-    while(index < file.length && isNaN(parseInt(file[index]))){
+    while(index<file.length && isNaN(parseInt(file[index]))){
         head+=file[index]
         index++
     }
-    // number 계산하기 -> 문자가 나올때까지 계산
-    while(index < file.length && number.length<5  && !isNaN(parseInt(file[index]))){
+    
+    while(index<file.length && number.length<=5 && !isNaN(parseInt(file[index]))){
         number+=file[index]
         index++
     }
@@ -19,13 +17,13 @@ function splitFile(file){
 }
 
 function solution(files) {
-     return files.sort((a,b)=>{
-         const [headA,numberA] = splitFile(a)
-         const [headB,numberB] = splitFile(b)
-         
-         const headComp = headA.toLowerCase().localeCompare(headB.toLowerCase())
-         if(headComp!==0) return headComp
-         
-         return Number(numberA) - Number(numberB)
-     })
+    return files.sort((a,b)=>{
+        const [headA,numberA] = splitFile(a)
+        const [headB,numberB] = splitFile(b)
+        
+        const headComp = headA.toLowerCase().localeCompare(headB.toLowerCase())
+        if(headComp!==0) return headComp
+        
+        return Number(numberA) - Number(numberB)
+    })
 }
