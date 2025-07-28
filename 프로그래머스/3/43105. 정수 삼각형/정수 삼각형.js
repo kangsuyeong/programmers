336815ef435da4a5eb48f3e7a93ffe67f796d2b5
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-28 15:32:05 +0900

    [level 3] Title: 정수 삼각형, Time: 7.64 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,18 +1,19 @@
 function solution(triangle) {
     const n = triangle.length
-    let dp = Array.from({length:n},()=>Array(n).fill(0))
+    const dp = Array.from({length:n},()=>Array(n).fill(0))
     
     dp[0][0] = triangle[0][0]
     
     for(let i=1;i<n;i++){
         for(let j=0;j<=i;j++){
             if(j===0){
-                dp[i][j] = dp[i-1][j] + triangle[i][j]
+                dp[i][0] = dp[i-1][0] + triangle[i][0]
             }
             else{
                 dp[i][j] = Math.max(dp[i-1][j-1],dp[i-1][j]) + triangle[i][j]
             }
         }
     }
-    return Math.max(...dp[n-1])
+    
+    return Math.max(...dp[n-1] )
 }
