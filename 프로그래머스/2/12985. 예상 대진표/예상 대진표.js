5b47ee9887c6a6569e8628dabe5fe131ddb20c00
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-07-29 12:18:12 +0900

    [level 2] Title: 예상 대진표, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,11 +1,11 @@
 function solution(n,a,b)
 {
-    let result = 0
+    let result = 0 // 라운드 수
     
     while(a!==b){
+        result+=1
         a = Math.ceil(a/2)
         b = Math.ceil(b/2)
-        result++
     }
     return result
 }
