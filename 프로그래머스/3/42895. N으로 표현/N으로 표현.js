68b8b676e1c829a9a3173830feb228e6d460493f
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-08 09:19:18 +0900

    [level 3] Title: N으로 표현, Time: 57.65 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,7 +1,8 @@
 function solution(N, number) {
-    
+    // dp[i] = i번 활용해서 만들수 있는 숫자 모음
     const dp = Array.from({length:9},()=>new Set())
     
+    // i=N을 사용한 횟수
     for(let i=1;i<=8;i++){
         dp[i].add(Number(N.toString().repeat(i)))
         
@@ -12,13 +13,11 @@ function solution(N, number) {
                     dp[i].add(x+y)
                     dp[i].add(x-y)
                     dp[i].add(x*y)
-                    if(y!==0) dp[i].add(Math.floor(x/y))
+                    if(y!==0) dp[i].add(x/y)
                 }
-            }
+            }    
         }
-        
         if(dp[i].has(number)) return i
     }
-    
     return -1
 }
