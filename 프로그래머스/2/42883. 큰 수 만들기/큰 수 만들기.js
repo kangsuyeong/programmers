38ee4a23b187be8170eef37c7c8eb7ceb40865c3
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-11 20:02:02 +0900

    [level 2] Title: 큰 수 만들기, Time: 32.96 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,9 +1,8 @@
 function solution(number, k) {
     const stack=[]
-    let index = 0
     
     for(const digits of number){
-        while(k>0 && stack.length>0 && stack[stack.length-1] <digits){
+        while(k>0 && stack.length>0 && stack[stack.length-1] < digits){
             stack.pop()
             k--
         }
