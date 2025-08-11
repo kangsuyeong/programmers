a8ffb1288e3298246af1e0bc6d5f467e95329169
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-11 19:30:35 +0900

    [level 3] Title: 기둥과 보 설치, Time: 278.62 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
@@ -1,46 +1,47 @@
 function solution(n, build_frame) {
-    const results = new Set()
     
-    // 기둥이 있는지
+    const s = new Set()
+    
+    // 기둥이 있는지 확인
     function hasP(x,y){
-        return results.has(`${x},${y},0`)
+        return s.has(`${x},${y},0`)
     }
     
-    // 보가 있는지
+    // 보가 있는지 확인
     function hasB(x,y){
-        return results.has(`${x},${y},1`)
+        return s.has(`${x},${y},1`)
     }
     
-    // 기둥 설치 유뮤
+    // 기둥 설치가 가능한지
     function canP(x,y){
         return y===0 || hasB(x,y) || hasB(x-1,y) || hasP(x,y-1)
     }
     
-    // 보 설치 유무
+    // 보 설치가 가능한지
     function canB(x,y){
-        return hasP(x,y-1) || hasP(x+1,y-1) ||(hasB(x-1,y) && hasB(x+1,y))
+        return hasP(x,y-1) || hasP(x+1,y-1) || (hasB(x-1,y) && hasB(x+1,y))
     }
     
     function isValid(){
-        for(const result of results){
-            const [x,y,a] = result.split(',').map(Number)
+        for(const item of s){
+            const [x,y,a] = item.split(',').map(Number)
             if(a===0 && !canP(x,y)) return false
             if(a===1 && !canB(x,y)) return false
         }
         return true
     }
     
-    
-    for(const [x,y,a,b] of build_frame){
+    for(const [x, y, a, b] of build_frame){
         const key = `${x},${y},${a}`
+        // 설치 경우
         if(b===1){
-            results.add(key)
-            if(!isValid()) results.delete(key)
+            s.add(key)
+            if(!isValid()) s.delete(key)
         }
         else{
-            results.delete(key)
-            if(!isValid()) results.add(key)
+            s.delete(key)
+            if(!isValid()) s.add(key)
         }
     }
-    return [...results].map(v=>v.split(',').map(Number)).sort((a,b)=>a[0]-b[0] || a[1]-b[1] || a[2]-b[2])
+    return [...s].map(v=>v.split(',').map(Number)).sort((a,b)=>a[0]-b[0]||a[1]-b[1] || a[2]-b[2])
 }
