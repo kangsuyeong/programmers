11e3da2c9522a65b4d3e6a3324d0e903714162a8
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-14 16:08:40 +0900

    [level 3] Title: 기둥과 보 설치, Time: 215.31 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
@@ -1,39 +1,32 @@
 function solution(n, build_frame) {
-    
     const s = new Set()
     
-    // 기둥이 있는지 확인
-    function hasP(x,y){
-        return s.has(`${x},${y},0`)
-    }
+    // 기둥 유무
+    const hasP = (x,y)=> s.has(`${x},${y},0`)
     
-    // 보가 있는지 확인
-    function hasB(x,y){
-        return s.has(`${x},${y},1`)
-    }
+    // 보 유무
+    const hasB = (x,y)=> s.has(`${x},${y},1`)
     
-    // 기둥 설치가 가능한지
-    function canP(x,y){
-        return y===0 || hasB(x,y) || hasB(x-1,y) || hasP(x,y-1)
-    }
+    // 기둥 설치 가능
+    const canP = (x,y)=> y===0 || hasB(x,y) || hasB(x-1,y) || hasP(x,y-1)
     
-    // 보 설치가 가능한지
-    function canB(x,y){
-        return hasP(x,y-1) || hasP(x+1,y-1) || (hasB(x-1,y) && hasB(x+1,y))
-    }
+    // 보 설치 가능
+    const canB = (x,y)=> hasP(x,y-1) || hasP(x+1,y-1) || (hasB(x-1,y) && hasB(x+1,y))
     
     function isValid(){
-        for(const item of s){
-            const [x,y,a] = item.split(',').map(Number)
+        const array = [...s].map(v=>v.split(',').map(Number))
+        for(const [x,y,a] of array){
+            // 기둥일때
             if(a===0 && !canP(x,y)) return false
             if(a===1 && !canB(x,y)) return false
         }
         return true
     }
     
+    
     for(const [x, y, a, b] of build_frame){
         const key = `${x},${y},${a}`
-        // 설치 경우
+        // 설치 일 경우
         if(b===1){
             s.add(key)
             if(!isValid()) s.delete(key)
@@ -43,5 +36,5 @@ function solution(n, build_frame) {
             if(!isValid()) s.add(key)
         }
     }
-    return [...s].map(v=>v.split(',').map(Number)).sort((a,b)=>a[0]-b[0]||a[1]-b[1] || a[2]-b[2])
+    return [...s].map(v=>v.split(',').map(Number)).sort((a,b)=>a[0]-b[0]||a[1]-b[1]||a[2]-b[2])
 }
