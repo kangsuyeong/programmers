d369b687f7970fc5bd6a3a27577fa43471f16094
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-14 14:57:19 +0900

    [level 2] Title: 조이스틱, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -0,0 +1,26 @@
+function solution(name) {
+    const n = name.length
+    
+    // 세로 이동
+    let vertical = 0
+    
+    for(let i=0;i<n;i++){
+        const diff = name.charCodeAt(i) - 65
+        vertical+=Math.min(diff,26-diff)
+    }
+    
+    let horizontal = n - 1;
+    for(let i=0;i<n;i++){
+        let next=i+1
+        if(name[next]!=='A') continue
+        
+        while(next<n && name[next]=='A') next++
+        
+        horizontal=Math.min(
+            horizontal,
+            i*2+(n-next),
+            (n-next)*2+i
+        )
+    }
+    return vertical + horizontal
+}
