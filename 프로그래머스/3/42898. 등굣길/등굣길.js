53c50dd2b933d239e5c7acaf3dfa4d2d571c46b8
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-15 09:25:54 +0900

    [level 3] Title: 등굣길, Time: 1.42 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,13 +1,16 @@
 function solution(m, n, puddles) {
+    
+    // dp[행][열]
     const dp = Array.from({length:n+1},()=>Array(m+1).fill(0))
     
+    // 물 웅덩이 표시 x=열 y=행
     for(const [x,y] of puddles){
         dp[y][x] = -1
     }
     dp[1][1] = 1
     for(let i=1;i<=n;i++){
         for(let j=1;j<=m;j++){
-            if(i===1 && j===1) continue
+            if(i===1 && j===1) continue // 시작점
             if(dp[i][j]===-1) continue
             
             const left = dp[i][j-1]===-1 ? 0:dp[i][j-1]
