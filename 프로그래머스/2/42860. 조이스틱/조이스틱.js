2fbc067152169c27429271d9fbfd1c4fe736f59b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-17 09:10:03 +0900

    [level 2] Title: 조이스틱, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,24 +1,21 @@
 function solution(name) {
     const n = name.length
-    let vertical = 0
     
-    // 수직 방향 계산
+    let vertical = 0
     for(let i=0;i<n;i++){
         const diff = name.charCodeAt(i) - 65
-        vertical += Math.min(diff,26-diff)
+        vertical+=Math.min(diff,26-diff)
     }
     
-    let horizontal = n-1 //오른쪽으로 쭉갔을때
+    let horizontal = n-1 // 오른쪽으로 쭉가는경우
     
-    
-    // 영향이 되는지점 A가 연속으로 있을때
-    // 유턴 지점
-    for(let i=0;i<n-1;i++){
+    // 영향을 받는 경우는 A가 연속으로 있을 경우
+    // 전환점
+    for(let i=0;i<n;i++){
         let next = i+1
         if(name[next]!=='A') continue
         
-        // 마지막 A 계산
-        while(name[next]==='A') next++
+        while(next<n && name[next]==='A') next++
         
         horizontal=Math.min(
             horizontal,
