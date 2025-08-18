6747d99520c105595afc7a0d67d76ec51d59fc7f
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-08-18 17:21:37 +0900

    [level 3] Title: 단속카메라, Time: 19.60 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,7 +1,6 @@
 function solution(routes) {
-    let result = 0
-    let index = -30001
-    
+    let result = 0 // 카메라 설치 갯수
+    let index = -300001 // 현재 카메라 설치 위치
     routes.sort((a,b)=>a[1]-b[1])
     
     for(const [start,end] of routes){
