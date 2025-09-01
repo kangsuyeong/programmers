5c09d57fa9e94da77f5496744ffc4967066ebe98
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-01 19:51:19 +0900

    [level 3] Title: N으로 표현, Time: 46.18 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,24 +1,28 @@
 function solution(N, number) {
+
     const dp = Array.from({length:9},()=>new Set())
     
     for(let i=1;i<=8;i++){
         dp[i].add(Number(N.toString().repeat(i)))
+        
         for(let j=1;j<=Math.floor(i/2);j++){
             for(const x of dp[j]){
                 for(const y of dp[i-j]){
                     dp[i].add(x+y)
+                    dp[i].add(x*y)
                     
                     dp[i].add(x-y)
                     dp[i].add(y-x)
                     
-                    dp[i].add(x*y)
-                    
-                    if(y!==0) dp[i].add(x/y)
                     if(x!==0) dp[i].add(y/x)
+                    if(y!==0) dp[i].add(x/y)
                 }
             }
         }
+        
         if(dp[i].has(number)) return i
     }
+    
+    
     return -1
 }
