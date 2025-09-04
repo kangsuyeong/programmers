7c467fcaeaacb11d16ecc42567438d5328e77a15
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-04 12:21:50 +0900

    [level 3] Title: 기둥과 보 설치, Time: 273.14 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
@@ -1,40 +1,50 @@
 function solution(n, build_frame) {
-    const s = new Set()
+    const result = new Set()
     
-    // 기둥 유무
-    const hasP = (x,y)=> s.has(`${x},${y},0`)
+    // 기둥 가지고 있는지
+    function hasP(x,y){
+        return result.has(`${x},${y},0`)
+    }
     
-    // 보 유무
-    const hasB = (x,y)=> s.has(`${x},${y},1`)
+    // 기둥을 설치할 수 있는지
+    function canP(x,y){
+        return y===0 || hasB(x,y) || hasB(x-1,y) || hasP(x,y-1)
+    }
     
-    // 기둥 설치 가능
-    const canP = (x,y)=> y===0 || hasB(x,y) || hasB(x-1,y) || hasP(x,y-1)
+    // 보를 설치할 수 있는지
+    function canB(x,y){
+        return hasP(x,y-1) || hasP(x+1,y-1) || (hasB(x-1,y) && hasB(x+1,y))
+    }
     
-    // 보 설치 가능
-    const canB = (x,y)=> hasP(x,y-1) || hasP(x+1,y-1) || (hasB(x-1,y) && hasB(x+1,y))
+    // 보를 가지고 있는지
+    function hasB(x,y){
+        return result.has(`${x},${y},1`)
+    }
+    
     
     function isValid(){
-        const array = [...s].map(v=>v.split(',').map(Number))
-        for(const [x,y,a] of array){
-            // 기둥일때
+        for(const item of result){
+            const [x,y,a] = item.split(',').map(Number)
             if(a===0 && !canP(x,y)) return false
             if(a===1 && !canB(x,y)) return false
         }
         return true
     }
     
-    
-    for(const [x, y, a, b] of build_frame){
+    for(const [x,y,a,b] of build_frame){
         const key = `${x},${y},${a}`
-        // 설치 일 경우
+        
+        // 설치일 경우
         if(b===1){
-            s.add(key)
-            if(!isValid()) s.delete(key)
+            result.add(key)
+            // 작업 불가능하면 롤백
+            if(!isValid()) result.delete(key)
         }
         else{
-            s.delete(key)
-            if(!isValid()) s.add(key)
+            result.delete(key)
+            // 작업 불가능하면 롤백
+            if(!isValid()) result.add(key)
         }
     }
-    return [...s].map(v=>v.split(',').map(Number)).sort((a,b)=>a[0]-b[0]||a[1]-b[1]||a[2]-b[2])
+    return [...result].map(v=>v.split(',').map(Number)).sort((a,b)=>a[0]-b[0] || a[1]-b[1] || a[2]-b[2])
 }
