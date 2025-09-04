7534fd738c8c2b6d3f49618cfe0b492227b14b1a
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-04 17:36:34 +0900

    [level 2] Title: N-Queen, Time: 446.37 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,24 +1,26 @@
 function solution(n) {
     let result = 0
-    const queen=[]
+    
+    const queens = []
     
     function isValid(row,col){
-        for(const [nr,nc] of queen){
-            if(col===nc || Math.abs(row-nr)===Math.abs(col-nc)) return false
+        for(const [nr,nc] of queens){
+            if(col===nc || Math.abs(nr-row)===Math.abs(nc-col)) return false
         }
         return true
     }
     
     function dfs(row){
         if(row===n){
-            result++
+            result+=1
             return
         }
+        
         for(let col=0;col<n;col++){
             if(isValid(row,col)){
-                queen.push([row,col])
+                queens.push([row,col])
                 dfs(row+1)
-                queen.pop()
+                queens.pop()
             }
         }
     }
