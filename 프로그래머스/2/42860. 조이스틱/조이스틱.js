3c4ed722c7d4249a07fe7defe78164c226c7120b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-04 18:44:01 +0900

    [level 2] Title: 조이스틱, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,24 +1,23 @@
 function solution(name) {
     const n = name.length
-    
     let vertical = 0
+    
     for(let i=0;i<n;i++){
         const diff = name.charCodeAt(i) - 65
-        vertical+=Math.min(diff,26-diff)
+        vertical += Math.min(diff,26-diff)
     }
     
-    let horizontal = n-1 
-    
-    // 회전점
+    let horizontal = n-1
     for(let i=0;i<n;i++){
-        let next=i+1
+        let next = i+1
+        if(name[next]!=='A') continue
         while(next<n && name[next]==='A') next++
         
         horizontal=Math.min(
             horizontal,
-            i*2+ (n-next),
+            i*2+(n-next),
             (n-next)*2 + i
         )
     }
-    return horizontal+ vertical
+    return vertical+ horizontal
 }
