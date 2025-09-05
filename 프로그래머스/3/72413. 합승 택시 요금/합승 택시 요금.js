7dd6d27887191c0d71ce4b54799f484a41b7e110
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-05 15:10:13 +0900

    [level 3] Title: 합승 택시 요금, Time: 88.32 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,25 +1,34 @@
 function solution(n, s, a, b, fares) {
-    const distance = Array.from({length:n+1},()=>Array(n+1).fill(Infinity))
+    const distacne = Array.from({length:n+1},()=>Array(n+1).fill(Infinity))
     
-    for(let i=1;i<=n;i++) distance[i][i] = 0
-    
-    for(const fare of fares){
-        const[start,end,cost] = fare
-        distance[start][end] = cost
-        distance[end][start] = cost
+    for(const [node1,node2,dist] of fares){
+        distacne[node1][node2] = dist
+        distacne[node2][node1] = dist
     }
     
+    // 플루이드 알고리즘으로 각 노드별 최소값 계산하기
+    // 경우점
     for(let k=1;k<=n;k++){
+        // 시작점
         for(let i=1;i<=n;i++){
+            // 끝점
             for(let j=1;j<=n;j++){
-                distance[i][j] = Math.min(distance[i][k] + distance[k][j],distance[i][j])
+                distacne[i][j] = Math.min(distacne[i][j],distacne[i][k] + distacne[k][j])
             }
         }
     }
     
+    for(let i=1;i<=n;i++){
+        for(let j=1;j<=n;j++){
+            if(i===j) distacne[i][j] = 0
+        }
+    }
+    
     let result = Infinity
-    for(let k=1;k<=n;k++){
-        result = Math.min(result,distance[s][k] + distance[k][a] + distance[k][b])
+    
+    // 합승 구간
+    for(let i=1;i<=n;i++){
+        result=Math.min(result,distacne[s][i]+distacne[i][a] + distacne[i][b],distacne[s][a]+distacne[s][b])
     }
     return result
 }
