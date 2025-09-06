381c007682ab35b9c161218b9d164d17dfec88a1
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-06 20:05:42 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 40.38 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
@@ -1,31 +1,35 @@
 function solution(alp, cop, problems) {
-    let max_alp = 0
-    let max_cop = 0
+    // 모든 문제를 풀기 위해서 최대 알고력과 최대 코딩력을 구해야한다.
+    let maxA = 0
+    let maxC = 0
     
-    // 최대 알고력, 코딩력 구하기
-    for(const [alp_req,cop_req] of problems){
-        max_alp = Math.max(alp_req,max_alp)
-        max_cop = Math.max(cop_req,max_cop)
+    for(const [alp_req, cop_req] of problems){
+        maxA = Math.max(maxA,alp_req)
+        maxC = Math.max(maxC,cop_req)
     }
     
-    const dp = Array.from({length:max_alp+2},()=>Array(max_cop+2).fill(Infinity))
-    alp = Math.min(alp,max_alp)
-    cop = Math.min(cop,max_cop)
+    const dp = Array.from({length:maxA+1},()=>Array(maxC+1).fill(Infinity))
+    
+    alp = Math.min(alp,maxA)
+    cop = Math.min(cop,maxC)
+    // 처음 시작
     dp[alp][cop] = 0
     
-    for(let i=alp;i<=max_alp;i++){
-        for(let j=cop;j<=max_cop;j++){
-            dp[i+1][j] = Math.min(dp[i+1][j],dp[i][j]+1)
-            dp[i][j+1] = Math.min(dp[i][j+1],dp[i][j]+1)
-
-            for(const [alp_req,cop_req,alp_rwd,cop_rwd,cost] of problems){
-                if(i>=alp_req && j>=cop_req){
-                    const next_alp = Math.min(max_alp,i+alp_rwd)
-                    const next_cop = Math.min(max_cop,j+cop_rwd)
-                    dp[next_alp][next_cop] = Math.min(dp[next_alp][next_cop],dp[i][j]+cost)
+    for(let a=alp;a<=maxA;a++){
+        for(let c=cop;c<=maxC;c++){
+            // 공부로 +1
+            if(a+1<=maxA) dp[a+1][c] = Math.min(dp[a+1][c],dp[a][c]+1)
+            if(c+1<=maxC) dp[a][c+1] = Math.min(dp[a][c+1],dp[a][c]+1)
+            
+            // 문제 풀기
+            for(const [alp_req, cop_req, alp_rwd, cop_rwd, cost] of problems){
+                if(a>=alp_req && c>=cop_req){
+                    const next_alp = Math.min(maxA,a+alp_rwd)
+                    const next_cop = Math.min(maxC,c+cop_rwd)
+                    dp[next_alp][next_cop] = Math.min(dp[next_alp][next_cop],dp[a][c] + cost)
                 }
             }
         }
     }
-    return dp[max_alp][max_cop ]
+    return dp[maxA][maxC]
 }
