a998826b3c906721a97ea3ce7847c75f5f64a6ca
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-09 14:23:30 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    const result = []
+    
+    while(n>0){
+        const r = n%3
+        if(r===0){
+            result.push(4)
+            n = Math.floor(n/3) - 1
+        }
+        else{
+            result.push(r)
+            n = Math.floor(n/3)
+        }
+    }
+    return result.reverse().join('')
+}
