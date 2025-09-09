8a4442d29a02294c88ba90ff15f97771ef781519
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-09 13:42:34 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 51.17 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,11 +1,14 @@
 function solution(numbers) {
-    const result = Array(numbers.length).fill(-1)
+    const n = numbers.length
+    const result = Array(n).fill(-1)
     const stack = []
     
-    for(let i=0;i<numbers.length;i++){
+    // 숫자가 아니라 인덱스를 담는다.
+    for(let i=0;i<n;i++){
+        // 현재 숫자
         const cur = numbers[i]
         
-        while(stack.length && numbers[stack[stack.length-1]] < cur){
+        while(stack.length && numbers[stack[stack.length-1]]<cur){
             const j = stack.pop()
             result[j] = cur
         }
