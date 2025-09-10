3c5a25b480cf57f3d968d0b07acbe6d5af46d4a5
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-10 17:16:42 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 41.54 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
@@ -9,30 +9,26 @@ function solution(alp, cop, problems) {
     
     const dp = Array.from({length:alp_max+1},()=>Array(cop_max+1).fill(Infinity))
     
-    
-    // 만약 현재 알고력이 문제푸는데 필요한 알고력 보다 높은 경우
-    alp=Math.min(alp,alp_max)
-    cop=Math.min(cop,cop_max)
+    alp=Math.min(alp_max,alp)
+    cop=Math.min(cop_max,cop)
     
     dp[alp][cop] = 0
     
     for(let a=alp;a<=alp_max;a++){
         for(let c=cop;c<=cop_max;c++){
-            if(a+1<=alp_max){
-                dp[a+1][c] = Math.min(dp[a+1][c],dp[a][c]+1)
-            }
             if(c+1<=cop_max){
                 dp[a][c+1] = Math.min(dp[a][c+1],dp[a][c]+1)
             }
+            if(a+1<=alp_max){
+                dp[a+1][c] = Math.min(dp[a+1][c],dp[a][c]+1)
+            }
             
-            // 문제 푸는 경우
             for(const [alp_req, cop_req, alp_rwd, cop_rwd, cost] of problems){
                 if(a>=alp_req && c>=cop_req){
                     const alp_next = Math.min(a+alp_rwd,alp_max)
                     const cop_next = Math.min(c+cop_rwd,cop_max)
                     dp[alp_next][cop_next] = Math.min(dp[alp_next][cop_next],dp[a][c]+cost)
                 }
-
             }
         }
     }
