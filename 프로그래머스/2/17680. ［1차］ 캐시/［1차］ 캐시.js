7a688af406a03688cea525e5a1be6dfb6e9df3ff
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-11 17:23:49 +0900

    [level 2] Title: [1차] 캐시, Time: 31.37 ms, Memory: 42.9 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,18 +1,17 @@
 function solution(cacheSize, cities) {
-    if(cacheSize===0) return cities.length*5 
-    
-    let result = 0
+    if(cacheSize===0) return cities.length*5
+    let result =0
     const cache = []
     
     for(const city of cities){
         const c = city.toLowerCase()
-        const index = cache.indexOf(c)
+        const idx = cache.indexOf(c)
         
         
-        // hit 일경우
-        if(index!==-1){
+        // hit
+        if(idx!==-1){
             result+=1
-            cache.splice(index,1)
+            cache.splice(idx,1)
             cache.push(c)
         }
         else{
