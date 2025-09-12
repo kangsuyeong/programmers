1b9378c5bee05799fd5a27713e68b10b60bdf43b
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-12 12:04:43 +0900

    [level 5] Title: 방의 개수, Time: 577.71 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.js
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -1,37 +1,36 @@
 function solution(arrows) {
-    let result = 0
-    const dx = [0, 1, 1, 1, 0, -1, -1, -1];
-    const dy = [1, 1, 0, -1, -1, -1, 0, 1];
+    const direction = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
     
-    const visitedNodes = new Set()
-    const visitedEdges = new Set()
+    const nodeVisited = new Set()
+    const edgeVisited = new Set()
+    
+    let result = 0
     
     let x = 0
     let y = 0
-    
-    visitedNodes.add(`0,0`)
-    
-    for(const dir of arrows){
+    nodeVisited.add(`0,0`)
+    for(const arrow of arrows){
+        
         for(let i=0;i<2;i++){
-            const nx = x + dx[dir]
-            const ny = y + dy[dir]
+            const [dx,dy] = direction[arrow]
+            const nx = x + dx
+            const ny = y + dy
             
-            const curNode = `${x},${y}`
-            const nextNode = `${nx},${ny}`
-            const edge = `${curNode}-${nextNode}`
-            const reverseEdge = `${nextNode}-${curNode}`
-            
-            // 다음 노드가 이미 방문했고, 처음가는 길일 경우 -> 방생성
-            if(visitedNodes.has(nextNode) && !visitedEdges.has(edge)){
+            const node = `${nx},${ny}`
+            const edge = `${x},${y}-${nx},${ny}`
+            const edgeReverse = `${nx},${ny}-${x},${y}`
+            if(nodeVisited.has(node) && !edgeVisited.has(edge)){
                 result++
             }
-            visitedNodes.add(nextNode)
-            visitedEdges.add(edge)
-            visitedEdges.add(reverseEdge)
             
+            nodeVisited.add(node)
+            edgeVisited.add(edge)
+            edgeVisited.add(edgeReverse)
             x = nx
-            y = ny
+            y= ny
+            
         }
+        
     }
     return result
 }
