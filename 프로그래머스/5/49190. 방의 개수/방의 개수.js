4107756b5fd76dc93345d57584c7d49fc05ac97a
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-13 21:02:03 +0900

    [level 5] Title: 방의 개수, Time: 584.08 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.js
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -4,33 +4,33 @@ function solution(arrows) {
     const nodeVisited = new Set()
     const edgeVisited = new Set()
     
-    let x = 0
-    let y = 0 
-    
-    nodeVisited.add(`0,0`)
+    let x=0
+    let y=0
+    nodeVisited.add('0,0')
     
     let result = 0
     
-   for(const arrow of arrows){
-       for(let i=1;i<=2;i++){
-           const [dx,dy] = direction[arrow]
-           const nx = dx + x
-           const ny = dy + y
-           
-           const currentNode = `${x},${y}`
-           const nextNode = `${nx},${ny}`
-           const edge = `${currentNode}-${nextNode}`
-           const reverEdge = `${nextNode}-${currentNode}`
-           
-           if(nodeVisited.has(nextNode) && !edgeVisited.has(edge)) result+=1
-           
-           nodeVisited.add(nextNode)
-           edgeVisited.add(edge)
-           edgeVisited.add(reverEdge)
-           
-           x = nx
-           y = ny
-       }
-   }
+    for(const arrow of arrows){
+        for(let i=0;i<2;i++){
+            const [dx,dy] = direction[arrow]
+            const nx = dx + x
+            const ny = dy + y
+            
+            const currentNode = `${x},${y}`
+            const nextNode = `${nx},${ny}`
+            const edge = `${currentNode}-${nextNode}`
+            const reverseEdge = `${nextNode}-${currentNode}`
+            
+            // 노드가 이미 방문했고, 처음 방문하는 엣지
+            if(nodeVisited.has(nextNode) && !edgeVisited.has(edge)) result++
+            
+            nodeVisited.add(nextNode)
+            edgeVisited.add(edge)
+            edgeVisited.add(reverseEdge)
+            
+            x = nx
+            y = ny
+        }
+    }
     return result
 }
