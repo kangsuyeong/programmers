d7f5818b547f8a7416b16ce3e23984fa7375964e
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-17 20:36:32 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 39.46 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js
@@ -3,32 +3,35 @@ function solution(alp, cop, problems) {
     let cop_max = 0
     
     for(const [alp_req, cop_req] of problems){
-        alp_max=Math.max(alp_max,alp_req)
-        cop_max=Math.max(cop_max,cop_req)
+        alp_max = Math.max(alp_max,alp_req)
+        cop_max = Math.max(cop_max,cop_req)
     }
     
     const dp = Array.from({length:alp_max+1},()=>Array(cop_max+1).fill(Infinity))
     
-    alp=Math.min(alp_max,alp)
-    cop=Math.min(cop_max,cop)
-    
+    alp=Math.min(alp,alp_max)
+    cop=Math.min(cop,cop_max)
     dp[alp][cop] = 0
     
-    for(let a=alp;a<=alp_max;a++){
-        for(let c=cop;c<=cop_max;c++){
-            if(c+1<=cop_max){
-                dp[a][c+1] = Math.min(dp[a][c+1],dp[a][c]+1)
-            }
+    for(let a = alp;a<=alp_max;a++){
+        for(let c = cop;c<=cop_max;c++){
+            
             if(a+1<=alp_max){
-                dp[a+1][c] = Math.min(dp[a+1][c],dp[a][c]+1)
+                dp[a+1][c] = Math.min(dp[a+1][c],dp[a][c] + 1)
             }
             
-            for(const [alp_req, cop_req, alp_rwd, cop_rwd, cost] of problems){
+            if(c+1<=cop_max){
+                dp[a][c+1] = Math.min(dp[a][c+1],dp[a][c] + 1)
+            }
+            
+            for(const  [alp_req, cop_req, alp_rwd, cop_rwd, cost] of problems){
                 if(a>=alp_req && c>=cop_req){
-                    const alp_next = Math.min(a+alp_rwd,alp_max)
-                    const cop_next = Math.min(c+cop_rwd,cop_max)
+                    const alp_next = Math.min(alp_max,alp_rwd+a)
+                    const cop_next = Math.min(cop_max,cop_rwd+c)
                     dp[alp_next][cop_next] = Math.min(dp[alp_next][cop_next],dp[a][c]+cost)
                 }
+                
+                
             }
         }
     }
