465f77897468617377332d20ffe8e05b36f96bb0
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-17 20:24:13 +0900

    [level 5] Title: 방의 개수, Time: 348.16 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.js
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -4,28 +4,31 @@ function solution(arrows) {
     const nodeVisited = new Set()
     const edgeVisited = new Set()
     
-    let x=0
-    let y=0
+    let x = 0
+    let y = 0
+    
     nodeVisited.add('0,0')
     
     let result = 0
     
+    
     for(const arrow of arrows){
+        const [dx,dy] = direction[arrow]
         for(let i=0;i<2;i++){
-            const [dx,dy] = direction[arrow]
-            const nx = dx + x
-            const ny = dy + y
+            const nx = x + dx
+            const ny = y + dy
+            
             
             const currentNode = `${x},${y}`
             const nextNode = `${nx},${ny}`
-            const edge = `${currentNode}-${nextNode}`
+            const edeg = `${currentNode}-${nextNode}`
             const reverseEdge = `${nextNode}-${currentNode}`
             
-            // 노드가 이미 방문했고, 처음 방문하는 엣지
-            if(nodeVisited.has(nextNode) && !edgeVisited.has(edge)) result++
-            
+            if(nodeVisited.has(nextNode) && !edgeVisited.has(edeg)){
+                result++   
+            }
             nodeVisited.add(nextNode)
-            edgeVisited.add(edge)
+            edgeVisited.add(edeg)
             edgeVisited.add(reverseEdge)
             
             x = nx
