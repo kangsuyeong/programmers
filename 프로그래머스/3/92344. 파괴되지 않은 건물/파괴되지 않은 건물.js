6d30eb4d08fd8eecc4dd0261818236d02c70c41d
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-19 22:19:13 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 115.53 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -1,47 +1,40 @@
 function solution(board, skill) {
     const n = board.length
     const m = board[0].length
-    
     const psum = Array.from({length:n+1},()=>Array(m+1).fill(0))
     
-    for(const s of skill){
-        const [type, r1, c1, r2, c2, degree] = s
-        const value = type===1 ? -degree : degree
+    for(const [type, r1, c1, r2, c2, degree] of skill){
+        const value = type===2 ?  degree: -degree
         
-        psum[r1][c1] +=value
-        psum[r1][c2+1] -=value
-        psum[r2+1][c1] -=value
+        psum[r1][c1] += value
+        psum[r1][c2+1] -= value
+        psum[r2+1][c1] -= value
         psum[r2+1][c2+1] +=value
     }
-    // 가로 누적합 전파
-    for(let i=0;i<=n;i++){
-        for(let j=1;j<=m;j++){
-            psum[i][j]+=psum[i][j-1]
+    
+    // 가로 전파
+    for(let row=0;row<n;row++){
+        for(let col=1;col<m;col++){
+            psum[row][col] +=psum[row][col-1]
         }
     }
     
-    // 세로 누적합 전파
-    for(let j=0;j<=m;j++){
-        for(let i=1;i<=n;i++){
-            psum[i][j]+=psum[i-1][j]
+    for(let col=0;col<m;col++){
+        for(let row=1;row<n;row++){
+            psum[row][col] +=psum[row-1][col]
         }
     }
     
-    // board 업데이트
-    for(let i=0;i<n;i++){
-        for(let j=0;j<m;j++){
-            board[i][j] +=psum[i][j]
+    for(let row=0;row<n;row++){
+        for(let col=0;col<m;col++){
+            board[row][col] +=psum[row][col]
         }
     }
-    
-    // count 계산하기
-    let count = 0
-    for(let i=0;i<n;i++){
-        for(let j=0;j<m;j++){
-            if(board[i][j]>0){
-                count++
-            }
+    let result=0
+    for(let row=0;row<n;row++){
+        for(let col=0;col<m;col++){
+            if(board[row][col]>0) result++
         }
     }
-    return count
+    return result
 }
