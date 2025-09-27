fb75226a937728a74efb7c6196f68d45f48e7fe4
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-09-27 12:34:20 +0900

    [level 4] Title: 지형 이동, Time: 203.93 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.js
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.js
@@ -2,9 +2,6 @@ class MinHeap{
     constructor(){
         this.items=[]
     }
-    size(){
-        return this.items.length
-    }
     push(item){
         this.items.push(item)
         this.bubbleUp()
@@ -16,16 +13,20 @@ class MinHeap{
         const min = this.items[0]
         this.items[0] = this.items.pop()
         this.bubbleDown()
+        
         return min
     }
     swap(a,b){
         [this.items[a],this.items[b]] = [this.items[b],this.items[a]]
     }
+    size(){
+        return this.items.length
+    }
     bubbleUp(){
-        let index = this.size()-1
+        let index = this.size() - 1
         while(index>0){
             const parentIndex = Math.floor((index-1)/2)
-            if(this.items[index][0]>=this.items[parentIndex][0]) break;
+            if(this.items[index][0] >= this.items[parentIndex][0]) break;
             
             this.swap(index,parentIndex)
             index = parentIndex
@@ -33,68 +34,48 @@ class MinHeap{
     }
     bubbleDown(){
         let index = 0
-        while(index*2 + 1<=this.size()-1){
-            const leftChild = index*2 + 1
-            const rightChild = index*2 + 2
-            const smallerChild = rightChild <= this.size()-1 && this.items[rightChild][0] < this.items[leftChild][0] ? rightChild : leftChild
+        while(index*2+1<=this.size()-1){
+            const leftIndex = index*2+1
+            const rightIndex = index*2+2
+            const smallerIndex = rightIndex<=this.size()-1 && this.items[rightIndex][0]<this.items[leftIndex][0] ? rightIndex : leftIndex
             
-            if(this.items[index][0] <= this.items[smallerChild][0]) break
+            if(this.items[index][0] <=this.items[smallerIndex][0]) break;
             
-            this.swap(index,smallerChild)
-            index = smallerChild
+            this.swap(index,smallerIndex)
+            index = smallerIndex
         }
     }
 }
 
 function solution(land, height) {
     const n = land.length
+    const minHeap = new MinHeap()
     const visited = Array.from({length:n},()=>Array(n).fill(false))
-    const dx = [0,0,1,-1]
-    const dy = [1,-1,0,0]
-    let result=0
+    
+    const direction = [[1,0],[0,-1],[-1,0],[0,1]]
     
     function isRange(x,y){
         return 0<=x && x<n && 0<=y && y<n
     }
     
-    const queue = new MinHeap()
-    queue.push([0,0,0])
-//        console.log('처음',queue)
-//     for(let i=0;i<10;i++){
-//                 const [cost,x,y] = queue.pop()
-//          console.log('pop한다음',queue)
-//         if(!visited[x][y]){
-//             visited[x][y] = true // 방문처리
-//             result+= cost
-//             for(let i=0;i<4;i++){
-//                 const nx = x+ dx[i]
-//                 const ny = y+dy[i]
-                
-//                 if(isRange(nx,ny) && !visited[nx][ny]){
-//                     const diff = Math.abs(land[x][y]-land[nx][ny])
-//                     const new_cost = diff > height ? diff : 0
-//                     queue.push([new_cost,nx,ny])
-//                 }
-//             }
-//         }
-//         console.log(queue)
-//     }
+    minHeap.push([0,0,0]) // [cost,x,y]
+    let result = 0
     
-    while(queue.size()>0){
-        const [cost,x,y] = queue.pop()
+    while(minHeap.size()>0){
+        const [cost,x,y] = minHeap.pop()
         
-        if(!visited[x][y]){
-            visited[x][y] = true // 방문처리
-            result+= cost
-            for(let i=0;i<4;i++){
-                const nx = x+ dx[i]
-                const ny = y+dy[i]
-                
-                if(isRange(nx,ny) && !visited[nx][ny]){
-                    const diff = Math.abs(land[x][y]-land[nx][ny])
-                    const new_cost = diff > height ? diff : 0
-                    queue.push([new_cost,nx,ny])
-                }
+        if(visited[x][y]) continue
+        
+        visited[x][y] = true
+        result+=cost
+        for(const [dx,dy] of direction){
+            const nx = x + dx
+            const ny = y + dy
+            
+            if(isRange(nx,ny) && !visited[nx][ny]){
+                const diff = Math.abs(land[x][y]-land[nx][ny])
+                const newCost = diff>height ? diff : 0
+                minHeap.push([newCost,nx,ny])
             }
         }
     }
