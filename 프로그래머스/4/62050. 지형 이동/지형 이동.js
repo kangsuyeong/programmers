e0731061987ea7e0d9114dbdba1e85544dfa61ff
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-02 14:00:12 +0900

    [level 4] Title: 지형 이동, Time: 226.04 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.js
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.js
@@ -25,7 +25,8 @@ class MinHeap{
         let index = this.size()-1
         while(index>0){
             const parentIndex = Math.floor((index-1)/2)
-            if(this.items[index][0] >= this.items[parentIndex][0]) break;
+            
+            if(this.items[index][0] >= this.items[parentIndex][0]) break
             
             this.swap(index,parentIndex)
             index = parentIndex
@@ -33,7 +34,7 @@ class MinHeap{
     }
     bubbleDown(){
         let index = 0
-        while(index*2+1 <= this.size()-1){
+        while(index*2 + 1 <=this.size()-1){
             const leftIndex = index*2 + 1
             const rightIndex = index*2 + 2
             const smallerIndex = rightIndex <= this.size()-1 && this.items[rightIndex][0] < this.items[leftIndex][0] ? rightIndex : leftIndex
@@ -41,33 +42,29 @@ class MinHeap{
             if(this.items[index][0] <= this.items[smallerIndex][0]) break;
             
             this.swap(index,smallerIndex)
-            index = smallerIndex
+            index= smallerIndex
         }
     }
 }
 
 function solution(land, height) {
     const n = land.length
-    const visited = Array.from({length:n},()=>Array(n).fill(false))
-    
     const direction = [[1,0],[-1,0],[0,1],[0,-1]]
-    
-    const minHeap = new MinHeap()
-    minHeap.push([0,0,0])
     
     function isRange(x,y){
         return 0<=x && x<n && 0<=y && y<n
     }
-    
+    const visited = Array.from({length:n},()=>Array(n).fill(false))
     let result = 0
+    
+    const minHeap = new MinHeap()
+    minHeap.push([0,0,0])
     
     while(minHeap.size()>0){
         const [cost,x,y] = minHeap.pop()
         
-        // 방문했으면 continue
         if(visited[x][y]) continue
         
-        // 방문처리
         visited[x][y] = true
         result+=cost
         
@@ -78,10 +75,10 @@ function solution(land, height) {
             if(!isRange(nx,ny) || visited[nx][ny]) continue
             
             const diff = Math.abs(land[x][y]-land[nx][ny])
-            const newCost = diff>height ? diff : 0
+            const newCost = diff > height ? diff : 0
             minHeap.push([newCost,nx,ny])
         }
-        
     }
+    
     return result
 }
