8dc99a839ea5c607dff0ae6d73086aecba2b9078
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-03 17:33:29 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 10.25 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,40 +1,37 @@
 function solution(n, m, x, y, r, c, k) {
-    let result = ''
-    const directions = [
-        [1,0,'d'],
-        [0,-1,'l'],
-        [0,1,'r'],
-        [-1,0,'u']
-    ]
+    const direction =[ ['d',1,0],['l',0,-1],['r',0,1],['u',-1,0]] // [str,dr,dc]
     
-    function isRange(cx,cy){
-        return cx>0 && cx<=n && cy>0 && cy<=m
+    let result = "impossible"
+    
+    function isRange(row,col){
+        return 1<=row && row<=n && 1<=col && col<=m
     }
     
-    function dfs(cx,cy,depth,string){
+    function dfs(row,col,depth,string){
+        if(result!=="impossible") return
+        
+        
+        const dist = Math.abs(row-r) + Math.abs(col-c)
+        const remain_move = k-depth
+        
+        if(dist > remain_move || (dist-remain_move)%2!==0) return
+        
+        
         if(depth===k){
-            if(cx===r && cy===c){
-                result = string
+            if(row===r && col===c){
+                result=string
             }
             return
         }
         
-        if(result) return // result 값이 이미 있다면 탐색 X
-        
-        const distance = Math.abs(cx-r) + Math.abs(cy-c) // 현재 남은 거리
-        const can_move = k-depth // 움직일 수 있는 횟수
-        
-        if(distance>can_move || (can_move-distance)%2!==0) return
-        
-        for(const [dx,dy,dir] of directions){
-            const nx = cx + dx
-            const ny = cy + dy
-            if(isRange(nx,ny)){
-                dfs(nx,ny,depth+1,string+dir)
-            }
+        for(const [str,dr,dc] of direction){
+            const nr = row + dr
+            const nc = col + dc
+            if(!isRange(nr,nc)) continue
+            
+            dfs(nr,nc,depth+1,string+str)
         }
-        
     }
     dfs(x,y,0,'')
-    return result ? result : 'impossible'
+    return result
 }
