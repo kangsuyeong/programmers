c48a126e24e84f32e3d7cb720eb5f5c1de19b48d
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-03 14:03:06 +0900

    [level 5] Title: 방의 개수, Time: 378.35 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.js
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -2,35 +2,35 @@ function solution(arrows) {
     const direction = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
     
     const nodeVisited = new Set()
-    const edgeVisited = new Set()
+    const edgeVistied = new Set()
     
+    // 현재위치
+    let x = 0
+    let y = 0
     
-    let x=0
-    let y=0
-    nodeVisited.add('0,0')
+    nodeVisited.add(`0,0`)
+    
     let result = 0
-    
     for(const arrow of arrows){
+        const [dx,dy] = direction[arrow]
         for(let i=0;i<2;i++){
-            const [dx,dy] = direction[arrow]
-            const nx = dx + x
-            const ny = dy + y
+            const nx = x + dx
+            const ny = y + dy
             
             const currentNode = `${x},${y}`
             const nextNode = `${nx},${ny}`
-            const edge = `${currentNode}-${nextNode}`
-            const reverseEdge = `${nextNode}-${currentNode}`
             
-            if(nodeVisited.has(nextNode) && !edgeVisited.has(edge)) result++
+            const edge = `${currentNode}-${nextNode}`
+            const edgeRerverse = `${nextNode}-${currentNode}`
+            if(nodeVisited.has(nextNode) && !edgeVistied.has(edge)) result++
             
             nodeVisited.add(nextNode)
-            edgeVisited.add(edge)
-            edgeVisited.add(reverseEdge)
+            edgeVistied.add(edge)
+            edgeVistied.add(edgeRerverse)
             
-            x=nx
+            x= nx
             y=ny
         }
-        
     }
     return result
 }
