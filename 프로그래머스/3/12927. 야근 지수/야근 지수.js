5b7e17d285c54a32f7864eaac063a2c4347e4957
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-05 14:30:44 +0900

    [level 3] Title: 야근 지수, Time: 155.49 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -27,7 +27,7 @@ class MaxHeap{
         while(index>0){
             const parentIndex = Math.floor((index-1)/2)
             
-            if(this.items[index] <= this.items[parentIndex]) break;
+            if(this.items[index] <= this.items[parentIndex]) break
             
             this.swap(index,parentIndex)
             index = parentIndex
@@ -35,12 +35,12 @@ class MaxHeap{
     }
     bubbleDown(){
         let index = 0
-        while(index*2+1<=this.size()-1){
+        while(index*2 + 1 <=this.size()-1){
             const leftIndex = index*2 + 1
             const rightIndex = index*2 + 2
             const biggerIndex = rightIndex <= this.size()-1 && this.items[rightIndex] > this.items[leftIndex] ? rightIndex : leftIndex
             
-            if(this.items[index] >= this.items[biggerIndex]) break
+            if(this.items[index] >=this.items[biggerIndex]) break;
             
             this.swap(index,biggerIndex)
             index = biggerIndex
@@ -49,37 +49,19 @@ class MaxHeap{
 }
 
 function solution(n, works) {
-    if(works.length===1){
-        const item = works[0] - n
-        if(item>0) return Math.pow(item,2)
-        else return 0
-    }
-    const maxHeap = new MaxHeap()
     
+    const totalWork = works.reduce((acc,cur)=>acc+cur,0)
+    if(totalWork <= n) return 0
+    
+    const maxHeap = new MaxHeap()
     for(const work of works){
         maxHeap.push(work)
     }
     
-    
-    while(n>0){
-        const maxWork = maxHeap.pop()
-        const maxSecondWork = maxHeap.items[0]
-        
-        
-        // 첫번째랑 두번째 차이
-        const diff = maxWork-maxSecondWork
-        
-        // 감소 시킬 양
-        const desc = Math.min(n,diff>0 ? diff : 1)
-        
-        if(maxWork-desc>0){
-            maxHeap.push(maxWork-desc)
-        }
-        else{
-            maxHeap.push(0)
-        }
-        n-=desc
-        
+
+    for(let i=0;i<n;i++){
+        const item = maxHeap.pop()
+        maxHeap.push(item-1)
     }
-     return maxHeap.items.reduce((acc,cur)=>acc+=Math.pow(cur,2),0)
+    return maxHeap.items.reduce((acc,cur)=>acc+Math.pow(cur,2),0)
 }
