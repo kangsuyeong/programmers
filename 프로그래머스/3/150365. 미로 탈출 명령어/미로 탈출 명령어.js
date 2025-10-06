c5c76e8f2e688241a6d4b19f6cd33f76dc496f67
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-06 21:33:16 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 316.84 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,38 +1,35 @@
 function solution(n, m, x, y, r, c, k) {
     const direction = [['d',1,0],['l',0,-1],['r',0,1],['u',-1,0]]
     
-    let result = "impossible"
-    
     function isRange(x,y){
         return 1<=x && x<=n && 1<=y && y<=m
     }
-    
+    let result = "impossible"
     function dfs(depth,x,y,string){
-        if(result !== "impossible") return
+        
+        if(result!=="impossible") return
         
         if(depth===k){
-            if(x===r && y===c) result = string
+            if(x===r && y===c) result=string
             return
         }
         
-        // 현재 남은거리
-        const dist = Math.abs(x-r) + Math.abs(y-c)
-        // 현재 남은 움직임 수
-        const remian_move = k- depth
         
-        if(dist>remian_move || (remian_move-dist)%2!==0) return
+        // 남은 거리
+        const dist = Math.abs(x-r) + Math.abs(y-c)
+        // 남은 움직임
+        const move = k-depth
+        
+        if(dist>move || (move-dist)%2!==0) return
         
         for(const [str,dx,dy] of direction){
             const nx = dx + x
             const ny = dy + y
             
             if(!isRange(nx,ny)) continue
-            
             dfs(depth+1,nx,ny,string+str)
         }
     }
-    
     dfs(0,x,y,'')
-    
     return result
 }
