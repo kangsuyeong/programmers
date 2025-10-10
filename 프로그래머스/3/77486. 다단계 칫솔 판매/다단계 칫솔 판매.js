0f7931cea17771310faa392e00dd17b8b019ae7d
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-10 20:17:47 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 44.60 ms, Memory: 49.1 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,38 +1,29 @@
 function solution(enroll, referral, seller, amount) {
     
-    // 전체 사람의 수
     const n = enroll.length
     
-    
-    // 추천인 정보를 저장하는 객체
     const referral_map = {}
-    
-    // 돈을 저장하는 객체
-    const money_map = {}
+    const total_map = {}
     
     for(let i=0;i<n;i++){
         referral_map[enroll[i]] = referral[i]
-        money_map[enroll[i]] = 0
+        total_map[enroll[i]] = 0
     }
-   
-    for(let i=0;i<seller.length;i++){
-        let 순이익 = amount[i]*100
-        let 판매원 = seller[i]
-        while(판매원!=='-'){
-            if(순이익<10){
-                money_map[판매원]+=순이익
+    
+    for(let i=0;i<amount.length;i++){
+        let 이익 = amount[i]*100
+        let 사람 = seller[i]
+        while(사람!=='-'){
+            if(이익<10){
+                total_map[사람]+=이익
                 break;
             }
             
-            let 바칠돈 = Math.floor(순이익*0.1)
-            money_map[판매원]+=순이익-바칠돈
-            
-            // 순이익 업데이트
-            순이익 =바칠돈
-            
-            판매원 = referral_map[판매원]
+            const 추천인지급이익 = Math.floor(이익*0.1)
+            total_map[사람] += 이익-추천인지급이익
+            이익 = 추천인지급이익
+            사람 = referral_map[사람]
         }
-
     }
-    return enroll.map((e)=>money_map[e])
+    return enroll.map(v=>total_map[v])
 }
