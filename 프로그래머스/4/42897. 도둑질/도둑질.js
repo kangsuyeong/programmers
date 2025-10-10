c01e76582e068b4b711ef76b9d3779e05c6c6175
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-10 20:53:56 +0900

    [level 4] Title: 도둑질, Time: 39.20 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.js
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -1,23 +1,22 @@
 function solution(money) {
     const n = money.length
+    const dp1= Array(n).fill(0)
+    const dp2= Array(n).fill(0)
     
-    const dp1 = Array(n).fill(-Infinity)
-    
-    const dp2 = Array(n).fill(-Infinity)
-    
+    // 첫집을 무조건 방문
     dp1[0] = money[0]
+    // 첫번째 집을 무조건 방문하니깐 두번째 집은 방문 못함
     dp1[1] = dp1[0]
     
     for(let i=2;i<n;i++){
         dp1[i] = Math.max(dp1[i-1],dp1[i-2]+money[i])
     }
     
-    dp2[0] = 0
     dp2[1] = money[1]
-    
     for(let i=2;i<n;i++){
         dp2[i] = Math.max(dp2[i-1],dp2[i-2]+money[i])
     }
     
     return Math.max(dp1[n-2],dp2[n-1])
+    
 }
