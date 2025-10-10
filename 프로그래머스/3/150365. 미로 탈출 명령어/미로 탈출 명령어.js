4fefb7dcef80e546bace54a4ce31eb2400fb90ba
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-10 21:29:33 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 12.35 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -4,29 +4,29 @@ function solution(n, m, x, y, r, c, k) {
     function isRange(x,y){
         return 1<=x && x<=n && 1<=y && y<=m
     }
+    
     let result = "impossible"
+    
     function dfs(depth,x,y,string){
         
-        if(result!=="impossible") return
+        if(result!=='impossible') return
         
         if(depth===k){
-            if(x===r && y===c) result=string
+            if(x===r && y===c) result = string
             return
         }
         
-        
-        // 남은 거리
         const dist = Math.abs(x-r) + Math.abs(y-c)
-        // 남은 움직임
-        const move = k-depth
+        const remain_move = k-depth
         
-        if(dist>move || (move-dist)%2!==0) return
+        if(dist>remain_move || (remain_move-dist)%2!==0) return
         
         for(const [str,dx,dy] of direction){
-            const nx = dx + x
-            const ny = dy + y
+            const nx = x+dx
+            const ny = y+dy
             
             if(!isRange(nx,ny)) continue
+            
             dfs(depth+1,nx,ny,string+str)
         }
     }
