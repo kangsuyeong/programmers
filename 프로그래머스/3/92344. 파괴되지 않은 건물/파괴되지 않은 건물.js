2d07e35352b9e7245a61700397ca3aeef1e6ecaa
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-14 18:29:04 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 75.15 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -1,6 +1,7 @@
 function solution(board, skill) {
     const n = board.length
     const m = board[0].length
+    
     const psum = Array.from({length:n+1},()=>Array(m+1).fill(0))
     
     for(const [type, r1, c1, r2, c2, degree] of skill){
@@ -8,28 +9,26 @@ function solution(board, skill) {
         
         psum[r1][c1] += value
         psum[r1][c2+1] -= value
-        psum[r2+1][c1] -= value
+        psum[r2+1][c1] -=value
         psum[r2+1][c2+1] += value
     }
     
-    // 가로 전파
+    // 가로 누적합
     for(let row=0;row<n;row++){
         for(let col=1;col<m;col++){
-            psum[row][col] +=psum[row][col-1]
+            psum[row][col] += psum[row][col-1]
         }
     }
     
     for(let col=0;col<m;col++){
         for(let row=1;row<n;row++){
-            psum[row][col] +=psum[row-1][col]
+            psum[row][col] += psum[row-1][col]
         }
     }
-    
     let result = 0
-    
     for(let row=0;row<n;row++){
         for(let col=0;col<m;col++){
-            if(board[row][col]+psum[row][col]>=1) result++
+            if(psum[row][col] + board[row][col]>=1) result++
         }
     }
     return result
