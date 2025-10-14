ace8614db1ff7c1a9289d509242600f859248f61
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-14 17:39:15 +0900

    [level 3] Title: 단어 변환, Time: 6.26 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -17,34 +17,39 @@ class Queue{
 }
 
 function solution(begin, target, words) {
+    // 단어 집합의 개수
     const n = words.length
-    const queue = new Queue()
-
+    
+    // 단어의 길이
+    const m = begin.length
+    if(!words.includes(target)) return 0
+    
     const visited = Array(n).fill(false)
     
+    const queue = new Queue()
     queue.push([begin,0])
+    
     while(queue.size()>0){
-        const [current_word,count] = queue.pop()
-
-        if(current_word===target) return count
+        const [current,depth] = queue.pop()
+        
+        if(current===target) return depth
+        
+        if(visited.includes(current)) continue
+        
         
         for(let i=0;i<n;i++){
-            if(visited[i]) continue
-            
-            const word_length = begin.length
-            let word_count = 0
-            for(let j=0;j<word_length;j++){
-                if(current_word[j]===words[i][j]) word_count++
+            // 문자 몇개나 같은지
+            let count =0
+            for(let j=0;j<m;j++){
+                if(current[j]===words[i][j]) count++
             }
             
-            if(word_length-1===word_count){
-                queue.push([words[i],count+1])
+            if(count===m-1){
+                queue.push([words[i],depth+1])
                 visited[i] = true
             }
-            
         }
-        
-        
     }
-    return 0
+    
+    
 }
