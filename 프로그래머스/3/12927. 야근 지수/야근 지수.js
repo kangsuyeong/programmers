4edb9c9dcf4f0f23e86e7cfd271d2a1e30a2c498
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-14 20:44:14 +0900

    [level 3] Title: 야근 지수, Time: 156.83 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -13,6 +13,7 @@ class MaxHeap{
         const top = this.items[0]
         this.items[0] = this.items.pop()
         this.bubbleDown()
+        
         return top
     }
     size(){
@@ -26,7 +27,7 @@ class MaxHeap{
         while(index>0){
             const parentIndex = Math.floor((index-1)/2)
             
-            if(this.items[index] <= this.items[parentIndex]) break;
+            if(this.items[index] <=this.items[parentIndex]) break;
             
             this.swap(index,parentIndex)
             index = parentIndex
@@ -34,12 +35,12 @@ class MaxHeap{
     }
     bubbleDown(){
         let index = 0
-        while(index*2+1 <= this.size()-1){
+        while(index*2 + 1 <= this.size()-1){
             const leftIndex = index*2 + 1
             const rightIndex = index*2 + 2
             const biggerIndex = rightIndex <= this.size()-1 && this.items[rightIndex] > this.items[leftIndex] ? rightIndex : leftIndex
             
-            if(this.items[index]>= this.items[biggerIndex]) break;
+            if(this.items[index] >= this.items[biggerIndex]) break;
             
             this.swap(index,biggerIndex)
             index = biggerIndex
@@ -48,8 +49,10 @@ class MaxHeap{
 }
 
 function solution(n, works) {
-    const totalWork = works.reduce((acc,cur)=>acc+cur,0)
-    if(totalWork<=n) return 0
+    
+    const total_work = works.reduce((acc,cur)=>acc+cur,0)
+    
+    if(total_work<=n) return 0
     
     const maxHeap = new MaxHeap()
     
@@ -61,7 +64,5 @@ function solution(n, works) {
         const item = maxHeap.pop()
         maxHeap.push(item-1)
     }
-    
     return maxHeap.items.reduce((acc,cur)=>acc+Math.pow(cur,2),0)
-    
 }
