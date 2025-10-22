73ff00f4c7658f0e40a3e387b71102abb2d3fdc2
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-22 20:51:15 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,55 +1,35 @@
-class Queue{
-    constructor(){
-        this.items=[]
-        this.rear=0
-        this.front=0
-    }
-    push(item){
-        this.items.push(item)
-        this.rear++
-    }
-    pop(){
-        return this.items[this.front++]
-    }
-    size(){
-        return this.rear-this.front
-    }
-}
-
 function solution(begin, target, words) {
-    // 단어 집합의 개수
-    const n = words.length
+    // 전체 단어 개수
+    const n= words.length
     
-    // 단어의 길이
+    // 문자열 길이
     const m = begin.length
-    if(!words.includes(target)) return 0
+    const visited = new Set()
     
-    const visited = Array(n).fill(false)
-    
-    const queue = new Queue()
+    const queue = []
     queue.push([begin,0])
     
-    while(queue.size()>0){
-        const [current,depth] = queue.pop()
-        
-        if(current===target) return depth
-        
-        if(visited.includes(current)) continue
-        
+    while(queue.length>0){
+        const [currentWord,depth] = queue.shift()
+        if(currentWord===target) return depth
         
         for(let i=0;i<n;i++){
-            // 문자 몇개나 같은지
+            const item = words[i]
+            if(visited.has(item)) continue
+            
+            // 몇개의 단어가 같은지
             let count =0
             for(let j=0;j<m;j++){
-                if(current[j]===words[i][j]) count++
+                if(currentWord[j]===item[j]) count++
             }
             
             if(count===m-1){
-                queue.push([words[i],depth+1])
-                visited[i] = true
+                queue.push([item,depth+1])
+                visited.add(item)
             }
         }
+        
+        
     }
-    
-    
+    return 0
 }
