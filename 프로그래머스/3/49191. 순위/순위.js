d3b990105fbdb082a3fca187fd9aeb5d5db69da7
강수영 <127009652+kangsuyeong@users.noreply.github.com>
2025-10-24 19:52:31 +0900

    [level 3] Title: 순위, Time: 12.46 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,35 +1,32 @@
-function dfs(graph,node,visited){
-    for(const next of graph[node] || []){
-        if(!visited.has(next)){
-            visited.add(next)
-            dfs(graph,next,visited)
+function solution(n, results) {
+    
+    // 승부를 확인하는 그래프
+    const graph = Array.from({length:n+1},()=>Array(n+1).fill(false))
+    
+    for(const [win,lose] of results){
+        graph[win][lose] = true
+    }
+    
+    for(let k=1;k<=n;k++){
+        for(let i=1;i<=n;i++){
+            for(let j=1;j<=n;j++){
+                if(graph[i][k] && graph[k][j]){
+                    graph[i][j] = true
+                }
+            }
         }
     }
-}
-
-function solution(n, results) {
-    const win = {}
-    const lose = {}
-    
-    for(const [winner,loser] of results){
-        win[winner] = win[winner] || []
-        lose[loser] = lose[loser] || []
-        
-        win[winner].push(loser)
-        lose[loser].push(winner)
-    }
-    
     let result = 0
-    
     for(let i=1;i<=n;i++){
-        const winSet = new Set()
-        const loseSet = new Set()
-        
-        dfs(win,i,winSet)
-        dfs(lose,i,loseSet)
-        
-        const total = winSet.size + loseSet.size
-        if(total==n-1) result++
+        let count = 0
+        for(let j=1;j<=n;j++){
+            if(graph[i][j] || graph[j][i]){
+                count++
+            }
+        }
+        if(count===n-1){
+            result++
+        }
     }
     return result
 }
